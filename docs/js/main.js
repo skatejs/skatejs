88c5f8337abee49af024d8aa87a0d622d5ee371c
Trey Shugart <treshugart@gmail.com>
2014-02-03 17:34:28 +1100

    anal alpha ordering

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,5 +3,5 @@ import externalLink from 'js/components/external-link';
 import icon from 'js/components/icon';
 
 skate('pre[code]', code);
-skate('i[icon]', icon);
 skate('a.external', externalLink);
+skate('i[icon]', icon);
