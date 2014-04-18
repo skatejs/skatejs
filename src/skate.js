8d079476663929a371f31dea88c11c36060bee38
Trey Shugart <treshugart@gmail.com>
2014-04-18 19:47:47 +1000

    Remove request animation frame stuff since it's not needed anymore.

--- src/skate.js
+++ src/skate.js
@@ -33,62 +33,6 @@
     }());
 
 
-  // `requestAnimationFrame` Polyfill
-  // --------------------------------
-
-  var timeout = (function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-
-    if (window.requestAnimationFrame) {
-      return window.requestAnimationFrame;
-    }
-
-    for (var x = 0; x < vendors.length; ++x) {
-      var method = vendors[x] + 'RequestAnimationFrame';
-
-      if (typeof window[method] === 'function') {
-        return window[method];
-      }
-    }
-
-    if (!window.requestAnimationFrame) {
-      return function(callback, element) {
-        var currTime = new Date().getTime();
-        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-        var id = window.setTimeout(function() {
-          callback(currTime + timeToCall);
-        },  timeToCall);
-
-        lastTime = currTime + timeToCall;
-
-        return id;
-      };
-    }
-  }());
-
-  // Ends a timeout started with `timeout`.
-  timeout.end = (function() {
-    if (window.cancelAnimationFrame) {
-      return window.cancelAnimationFrame;
-    }
-
-    return function(id) {
-      clearTimeout(id);
-    };
-  })();
-
-  // Repeats the specified `fn` until `timeout` is ended.
-  timeout.repeat = function(fn) {
-    var call = function() {
-      fn();
-      return timeout(call);
-    };
-
-    return call();
-  };
-
-
   // Factory
   // -------
 
