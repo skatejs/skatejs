c680e691767053aea380ae7e01c5e36a7354c8bc
Trey Shugart <treshugart@gmail.com>
2014-09-17 16:45:55 +1000

    Implement testing with Sauce Labs. Initiallly test one version of Chrome, Firefox and IE.

--- build/configs/karma.js
+++ build/configs/karma.js
@@ -1,15 +1,40 @@
 module.exports = function (grunt) {
   var browsers = grunt.option('browsers');
+  var sauce = grunt.option('sauce');
+  var customLaunchers = {
+      sl_chrome: {
+        base: 'SauceLabs',
+        browserName: 'Chrome',
+        platform: 'Windows 8.1',
+        version: '37'
+      },
+      sl_firefox: {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'Windows 8.1',
+        version: '31'
+      },
+      sl_ie_11: {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 8.1',
+        version: '11'
+      }
+    };
 
-  if (browsers) {
+  if (sauce) {
+    browsers = Object.keys(customLaunchers);
+  } else if (browsers) {
     browsers = browsers.split(',');
   } else {
-    browsers = ['PhantomJS'];
+    browsers = ['Chrome'];
   }
 
   return {
     options: {
       browsers: browsers,
+
+      customLaunchers: customLaunchers,
 
       files: [
         { pattern: '.tmp/run-unit-tests.js', included: true }
@@ -27,10 +52,20 @@ module.exports = function (grunt) {
         'karma-chrome-launcher',
         'karma-firefox-launcher',
         'karma-mocha',
-        'karma-phantomjs-launcher'
+        'karma-phantomjs-launcher',
+        'karma-sauce-launcher'
       ],
 
       port: grunt.option('port') || '9876',
+
+      reporters: [
+        'progress',
+        'saucelabs'
+      ],
+
+      saucelabs: {
+        testName: 'SkateJS Unit Tests'
+      },
 
       singleRun: !grunt.option('keep-alive'),
     },
