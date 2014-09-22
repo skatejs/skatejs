0d755b2814c3e78b71e6e99519ccef4759354e88
Trey Shugart <treshugart@gmail.com>
2014-09-22 11:59:23 +1000

    Fix watch task patterns.

--- build/configs/watch.js
+++ build/configs/watch.js
@@ -1,11 +1,14 @@
 module.exports = function (grunt) {
+  var src = 'src/{*,**/*}.{js,less}';
+  var test = 'test/{*,**/*}.{js,less}';
+
   return {
     dist: {
-      files: ['src/*.js'],
-      tasks: ['shell:dist', 'uglify:dist']
+      files: [src],
+      tasks: ['less:dist', 'shell:dist', 'uglify:dist']
     },
     test: {
-      files: ['src/*.js', 'test/{*,**.js}'],
+      files: [src, test],
       tasks: ['shell:test']
     }
   };
