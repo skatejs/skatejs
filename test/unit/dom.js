84adda75ef6d7bfdf95d7b491ebcfd17ec1c1510
Trey Shugart <treshugart@gmail.com>
2014-09-22 12:01:45 +1000

    Note IE problems in remove tests.

--- test/unit/dom.js
+++ test/unit/dom.js
@@ -26,11 +26,13 @@ describe('DOM', function () {
       assertComponent('sub-element', 'insert', '<div><child><sub-element></sub-element></child></div>', done);
     });
 
+    // IE 11 has a bug: https://connect.microsoft.com/IE/feedback/details/817132/ie-11-childnodes-are-missing-from-mutationobserver-mutations-removednodes-after-setting-innerhtml.
     it('should pick up descendants that are removed if an ancestor\'s innerHTML is set.', function (done) {
       assertComponent('sub-element', 'remove', '<div id="removing"><child><sub-element></sub-element></child></div>', done);
       helpers.fixture('');
     });
 
+    // IE 9/10 have issues with this. This is also to ensure IE 11 doesn't.
     it('should pick up descendants that are removed if an ancestor is removed.', function (done) {
       assertComponent('sub-element', 'remove', '<div id="removing"><child><sub-element></sub-element></child></div>', done);
       var div = helpers.fixture().querySelector('div');
