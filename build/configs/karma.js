a6880434c5699141e5105066eec6867bbdc5c37e
Trey Shugart <treshugart@gmail.com>
2014-09-23 10:00:40 +1000

    List current supported browsers.

--- build/configs/karma.js
+++ build/configs/karma.js
@@ -1,25 +1,47 @@
 module.exports = function (grunt) {
+  'use strict';
+
+  function launcher (platform, browser, version) {
+    var device = '';
+
+    if (browser === 'iphone') {
+      device = 'iPhone';
+    }
+
+    if (browser === 'android') {
+      device = 'Android';
+    }
+
+    return {
+      base: 'SauceLabs',
+      platform: platform,
+      browserName: browser,
+      version: version && version.toString() || '',
+      deviceName: device,
+      'device-orientation': device ? 'portrait' : ''
+    };
+  }
+
   var browsers = grunt.option('browsers');
   var sauce = grunt.option('sauce');
   var customLaunchers = {
-      sl_chrome: {
-        base: 'SauceLabs',
-        browserName: 'chrome',
-        platform: 'Windows 8.1',
-        version: '37'
-      },
-      sl_firefox: {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        platform: 'Windows 8.1',
-        version: '31'
-      },
-      sl_ie_11: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 8.1',
-        version: '11'
-      }
+      chrome_37: launcher('Windows 8.1', 'chrome', 37),
+      chrome_36: launcher('Windows 8.1', 'chrome', 36),
+
+      firefox_32: launcher('Windows 8.1', 'firefox', 32),
+      firefox_31: {launcher('Windows 8.1', 'firefox', 31),
+
+      safari_7: launcher('OS X 10.9', 'safari', 7),
+      safari_6: launcher('OS X 10.8', 'safari', 6),
+
+      ios_safari_7_1: launcher('OS X 10.9', 'iphone', 7.1),
+      ios_safari_7: launcher('OS X 10.9', 'iphone', 7),
+      ios_safari_6_1: launcher('OS X 10.8', 'iphone', 6.1),
+      ios_safari_6: launcher('OS X 10.8', 'iphone', 6),
+
+      ie_11: launcher('Windows 8.1', 'internet explorer', 11),
+      ie_10: launcher('Windows 8', 'internet explorer', 10),
+      ie_9: launcher('Windows 7', 'internet explorer', 9)
     };
 
   if (sauce) {
