bbe0838b270c67391e404dd406ca199f4f0d054e
Trey Shugart <treshugart@gmail.com>
2014-10-20 12:03:36 +1100

    Fix invalid method call.

--- build/lib/version.js
+++ build/lib/version.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
   version.next = function (type) {
     return grunt.option('version') ||
       semver.inc(
-        version.current(),
+        version(),
         grunt.option('type') || 'patch'
       );
   };
