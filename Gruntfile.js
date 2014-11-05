0559036496178af2249d7264124658f8930f29ba
Trey Shugart <treshugart@gmail.com>
2014-11-05 16:30:40 +1100

    Improve compile() function.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,6 @@ module.exports = function (grunt) {
   grunt.initConfig(config);
   grunt.loadTasks('build/tasks');
   grunt.loadNpmTasks('grunt-available-tasks');
-  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
--- build/lib/compile.js
+++ build/lib/compile.js
@@ -1,13 +1,12 @@
 module.exports = function (src, dest) {
+  var cmd = require('./cmd');
   var path = require('path');
 
-  return '' +
-    'node' +
-    ' ./node_modules/6to5/bin/6to5/index.js ' + path.dirname(src) +
-    ' --out-dir .tmp/6to5' +
-    ' &&' +
-    ' mkdir -p ' + path.dirname(dest) +
-    ' &&' +
-    ' node ./node_modules/browserify/bin/cmd.js .tmp/6to5/' + path.basename(src) +
-    ' -o ' + dest;
+  return cmd(
+    'rm -rf .tmp/6to5',
+    'mkdir -p .tmp/6to5',
+    './node_modules/.bin/6to5 ' + path.dirname(src) + ' --out-dir .tmp/6to5',
+    'mkdir -p ' + path.dirname(dest),
+    './node_modules/.bin/browserify .tmp/6to5/' + path.basename(src) + ' -o ' + dest
+  );
 };
