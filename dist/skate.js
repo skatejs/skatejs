e7319d6d9332ba227d103a7c93914c37d03e6bfc
Trey Shugart <treshugart@gmail.com>
2015-01-27 16:14:41 +1100

    Update dist.

--- dist/skate.js
+++ dist/skate.js
@@ -1,262 +1,15 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/* (The MIT License)
- *
- * Copyright (c) 2012 Brandon Benvie <http://bbenvie.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the 'Software'), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish, distribute,
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included with all copies or
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
- * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
-// Original WeakMap implementation by Gozala @ https://gist.github.com/1269991
-// Updated and bugfixed by Raynos @ https://gist.github.com/1638059
-// Expanded by Benvie @ https://github.com/Benvie/harmony-collections
-
-void function(global, undefined_, undefined){
-  var getProps = Object.getOwnPropertyNames,
-      defProp  = Object.defineProperty,
-      toSource = Function.prototype.toString,
-      create   = Object.create,
-      hasOwn   = Object.prototype.hasOwnProperty,
-      funcName = /^\n?function\s?(\w*)?_?\(/;
-
-
-  function define(object, key, value){
-    if (typeof key === 'function') {
-      value = key;
-      key = nameOf(value).replace(/_$/, '');
-    }
-    return defProp(object, key, { configurable: true, writable: true, value: value });
-  }
-
-  function nameOf(func){
-    return typeof func !== 'function'
-          ? '' : 'name' in func
-          ? func.name : toSource.call(func).match(funcName)[1];
-  }
-
-  // ############
-  // ### Data ###
-  // ############
-
-  var Data = (function(){
-    var dataDesc = { value: { writable: true, value: undefined } },
-        datalock = 'return function(k){if(k===s)return l}',
-        uids     = create(null),
-
-        createUID = function(){
-          var key = Math.random().toString(36).slice(2);
-          return key in uids ? createUID() : uids[key] = key;
-        },
-
-        globalID = createUID(),
-
-        storage = function(obj){
-          if (hasOwn.call(obj, globalID))
-            return obj[globalID];
-
-          if (!Object.isExtensible(obj))
-            throw new TypeError("Object must be extensible");
-
-          var store = create(null);
-          defProp(obj, globalID, { value: store });
-          return store;
-        };
-
-    // common per-object storage area made visible by patching getOwnPropertyNames'
-    define(Object, function getOwnPropertyNames(obj){
-      var props = getProps(obj);
-      if (hasOwn.call(obj, globalID))
-        props.splice(props.indexOf(globalID), 1);
-      return props;
-    });
-
-    function Data(){
-      var puid = createUID(),
-          secret = {};
-
-      this.unlock = function(obj){
-        var store = storage(obj);
-        if (hasOwn.call(store, puid))
-          return store[puid](secret);
-
-        var data = create(null, dataDesc);
-        defProp(store, puid, {
-          value: new Function('s', 'l', datalock)(secret, data)
-        });
-        return data;
-      }
-    }
-
-    define(Data.prototype, function get(o){ return this.unlock(o).value });
-    define(Data.prototype, function set(o, v){ this.unlock(o).value = v });
-
-    return Data;
-  }());
-
-
-  var WM = (function(data){
-    var validate = function(key){
-      if (key == null || typeof key !== 'object' && typeof key !== 'function')
-        throw new TypeError("Invalid WeakMap key");
-    }
-
-    var wrap = function(collection, value){
-      var store = data.unlock(collection);
-      if (store.value)
-        throw new TypeError("Object is already a WeakMap");
-      store.value = value;
-    }
-
-    var unwrap = function(collection){
-      var storage = data.unlock(collection).value;
-      if (!storage)
-        throw new TypeError("WeakMap is not generic");
-      return storage;
-    }
-
-    var initialize = function(weakmap, iterable){
-      if (iterable !== null && typeof iterable === 'object' && typeof iterable.forEach === 'function') {
-        iterable.forEach(function(item, i){
-          if (item instanceof Array && item.length === 2)
-            set.call(weakmap, iterable[i][0], iterable[i][1]);
-        });
-      }
-    }
-
-
-    function WeakMap(iterable){
-      if (this === global || this == null || this === WeakMap.prototype)
-        return new WeakMap(iterable);
-
-      wrap(this, new Data);
-      initialize(this, iterable);
-    }
-
-    function get(key){
-      validate(key);
-      var value = unwrap(this).get(key);
-      return value === undefined_ ? undefined : value;
-    }
-
-    function set(key, value){
-      validate(key);
-      // store a token for explicit undefined so that "has" works correctly
-      unwrap(this).set(key, value === undefined ? undefined_ : value);
-    }
-
-    function has(key){
-      validate(key);
-      return unwrap(this).get(key) !== undefined;
-    }
-
-    function delete_(key){
-      validate(key);
-      var data = unwrap(this),
-          had = data.get(key) !== undefined;
-      data.set(key, undefined);
-      return had;
-    }
-
-    function toString(){
-      unwrap(this);
-      return '[object WeakMap]';
-    }
-
-    try {
-      var src = ('return '+delete_).replace('e_', '\\u0065'),
-          del = new Function('unwrap', 'validate', src)(unwrap, validate);
-    } catch (e) {
-      var del = delete_;
-    }
-
-    var src = (''+Object).split('Object');
-    var stringifier = function toString(){
-      return src[0] + nameOf(this) + src[1];
-    };
-
-    define(stringifier, stringifier);
-
-    var prep = { __proto__: [] } instanceof Array
-      ? function(f){ f.__proto__ = stringifier }
-      : function(f){ define(f, stringifier) };
-
-    prep(WeakMap);
-
-    [toString, get, set, has, del].forEach(function(method){
-      define(WeakMap.prototype, method);
-      prep(method);
-    });
-
-    return WeakMap;
-  }(new Data));
-
-  var defaultCreator = Object.create
-    ? function(){ return Object.create(null) }
-    : function(){ return {} };
-
-  function createStorage(creator){
-    var weakmap = new WM;
-    creator || (creator = defaultCreator);
-
-    function storage(object, value){
-      if (value || arguments.length === 2) {
-        weakmap.set(object, value);
-      } else {
-        value = weakmap.get(object);
-        if (value === undefined) {
-          value = creator(object);
-          weakmap.set(object, value);
-        }
-      }
-      return value;
-    }
-
-    return storage;
-  }
-
-
-  if (typeof module !== 'undefined') {
-    module.exports = WM;
-  } else if (typeof exports !== 'undefined') {
-    exports.WeakMap = WM;
-  } else if (!('WeakMap' in global)) {
-    global.WeakMap = WM;
-  }
-
-  WM.createStorage = createStorage;
-  if (global.WeakMap)
-    global.WeakMap.createStorage = createStorage;
-}((0, eval)('this'));
-
-},{}],2:[function(require,module,exports){
 "use strict";
 
 var ATTR_IGNORE = exports.ATTR_IGNORE = "data-skate-ignore";
-},{}],3:[function(require,module,exports){
+},{}],2:[function(require,module,exports){
 "use strict";
 
-require('../node_modules/weakmap/weakmap');
-
-var map = new WeakMap();
-
 exports.default = function (element) {
-  var data = map.get(element);
+  var data = element.__SKATE_DATA;
 
   if (!data) {
-    data = {};
-    map.set(element, data);
+    element.__SKATE_DATA = data = {};
   }
 
   return {
@@ -270,7 +23,7 @@ exports.default = function (element) {
     }
   };
 };
-},{"../node_modules/weakmap/weakmap":1}],4:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 "use strict";
 
 var globals = require('./globals').default;
@@ -353,7 +106,7 @@ exports.default = {
     return this;
   }
 };
-},{"./globals":5,"./lifecycle":6,"./mutation-observer":7,"./utils":10}],5:[function(require,module,exports){
+},{"./globals":4,"./lifecycle":5,"./mutation-observer":6,"./utils":9}],4:[function(require,module,exports){
 "use strict";
 
 if (!window.__skate) {
@@ -364,7 +117,7 @@ if (!window.__skate) {
 }
 
 exports.default = window.__skate;
-},{}],6:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 "use strict";
 
 var ATTR_IGNORE = require('./constants').ATTR_IGNORE;
@@ -623,11 +376,12 @@ function triggerCreated(target, component) {
 
   // We use the unresolved / resolved attributes to flag whether or not the
   // element has been templated or not.
-  if (component.template && !targetData.get("templated")) {
+  if (component.template && !target.hasAttribute(component.resolvedAttribute)) {
     component.template(target);
-    targetData.set("templated", true);
   }
 
+  target.removeAttribute(component.unresolvedAttribute);
+  target.setAttribute(component.resolvedAttribute, "");
   addEventListeners(target, component);
   addAttributeListeners(target, component);
   addAttributeToPropertyLinks(target, component);
@@ -659,8 +413,6 @@ function triggerAttached(target, component) {
   }
 
   targetData.set("attached", true);
-  target.removeAttribute(component.unresolvedAttribute);
-  target.setAttribute(component.resolvedAttribute, "");
 
   if (component.attached) {
     component.attached(target);
@@ -774,7 +526,7 @@ exports.triggerAttached = triggerAttached;
 exports.triggerAttributeChanged = triggerAttributeChanged;
 exports.triggerCreated = triggerCreated;
 exports.triggerDetached = triggerDetached;
-},{"./constants":2,"./data":3,"./mutation-observer":7,"./registry":8,"./utils":10}],7:[function(require,module,exports){
+},{"./constants":1,"./data":2,"./mutation-observer":6,"./registry":7,"./utils":9}],6:[function(require,module,exports){
 "use strict";
 
 var debounce = require('./utils').debounce;
@@ -1091,7 +843,7 @@ MutationObserver.prototype = {
 };
 
 exports.default = MutationObserver;
-},{"./utils":10}],8:[function(require,module,exports){
+},{"./utils":9}],7:[function(require,module,exports){
 "use strict";
 
 var globals = require('./globals').default;
@@ -1201,7 +953,7 @@ exports.default = {
     return this;
   }
 };
-},{"./globals":5,"./utils":10}],9:[function(require,module,exports){
+},{"./globals":4,"./utils":9}],8:[function(require,module,exports){
 "use strict";
 
 var documentObserver = require('./document-observer').default;
@@ -1435,7 +1187,7 @@ if (typeof exports === "object") {
 }
 
 exports.default = skate;
-},{"./document-observer":4,"./lifecycle":6,"./registry":8,"./utils":10,"./version":11}],10:[function(require,module,exports){
+},{"./document-observer":3,"./lifecycle":5,"./registry":7,"./utils":9,"./version":10}],9:[function(require,module,exports){
 "use strict";
 
 exports.hasOwn = hasOwn;
@@ -1517,8 +1269,8 @@ function objEach(obj, fn) {
 function supportsNativeCustomElements() {
   return typeof document.registerElement === "function";
 }
-},{"./constants":2}],11:[function(require,module,exports){
+},{"./constants":1}],10:[function(require,module,exports){
 "use strict";
 
 exports.default = "0.13.0";
-},{}]},{},[9]);
+},{}]},{},[8]);
