b387eab32ad96de20cb3400f85249da4889bfa4a
Will Binns-Smith <wbinnssmith@atlassian.com>
2015-02-07 21:02:04 -0800

    Add browserify-deqrequire to build to not confuse loaders.

--- build/lib/compile.js
+++ build/lib/compile.js
@@ -10,6 +10,6 @@ var to5ify = require('6to5ify');
 module.exports = function (src, dest, name) {
   sh.mkdir('-p', path.dirname(dest));
 
-  var opts = './' + src + ' -t 6to5ify -o ' + dest;
+  var opts = './' + src + ' -t 6to5ify -p browserify-derequire -o ' + dest;
   sh.exec('./node_modules/.bin/browserify ' + opts);
 };
