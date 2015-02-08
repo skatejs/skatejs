299b34629036d2b330af84e8cb14372f1b651863
Will Binns-Smith <wbinnssmith@atlassian.com>
2015-02-07 23:25:32 -0800

    Use mold to transform the sourcemap root.

--- build/lib/compile.js
+++ build/lib/compile.js
@@ -8,6 +8,7 @@ var browserify = require('browserify');
 var to5ify = require('6to5ify');
 var derequire = require('browserify-derequire');
 var exorcist = require('exorcist');
+var mold = require('mold-source-map');
 
 module.exports = function (src, dest, name, cb) {
   sh.mkdir('-p', path.dirname(dest));
@@ -26,6 +27,7 @@ module.exports = function (src, dest, name, cb) {
     .bundle();
 
   bundle
+    .pipe(mold.transformSourcesRelativeTo(path.join(__dirname, '..', '..')))
     .pipe(exorcist(dest + '.map'))
     .pipe(out);
 
