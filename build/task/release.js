c7ec474184c8792590da2210af5ef50fbc7497ad
Trey Shugart <treshugart@gmail.com>
2015-03-18 13:52:35 +1100

    Fix release script.

--- build/task/release.js
+++ build/task/release.js
@@ -7,22 +7,18 @@ var semver = require('semver');
 var sh = require('shelljs');
 
 cmd
-  .option('-s, --skip-tests', 'Skips running tests.')
-  .option('-v, --version [version]', 'The version to release in lieu of --type.')
-  .option('-t, --type [major, minor or patch]', 'The type of release being performed.')
+  .option('-s, --semver [version]', 'The semantic version to release in lieu of --type. This takes precedence over --type.')
+  .option('-t, --type [major, minor or patch]', 'The type of release being performed in lieu of --semver.')
   .parse(process.argv);
 
 module.exports = function () {
   var currentVersion = pkg.version;
-  var nextVersion = cmd.version || semver.inc(
+  var nextVersion = cmd.semver || semver.inc(
     currentVersion,
     cmd.type || 'patch'
   );
 
-  if (!cmd.skipTests) {
-    sh.exec('npm run test');
-  }
-
+  sh.exec('npm run test');
   replace('src/version.js', currentVersion, nextVersion);
   replace('bower.json', currentVersion, nextVersion);
   replace('package.json', currentVersion, nextVersion);
