59cd2183955d93ab6eddac86849a15aff8687828
Trey Shugart <treshugart@gmail.com>
2015-03-18 12:39:49 +1100

    Disable jscs linting until they support ES6.

--- build/task/lint.js
+++ build/task/lint.js
@@ -4,5 +4,6 @@ var sh = require('shelljs');
 
 module.exports = function () {
   sh.exec('./node_modules/.bin/jshint build src test');
-  sh.exec('./node_modules/.bin/jscs build src test');
+  // Enable once ES6 support lands.
+  // sh.exec('./node_modules/.bin/jscs build src test');
 };
