f26157f4c875dca1fcb55523087aa18136e4236f
Trey Shugart <treshugart@gmail.com>
2015-05-22 16:18:11 +1000

    Remove old build directory.

--- build/lib/replace.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var fs = require('fs');
-
-module.exports = function (file, pattern, replacement) {
-  var str = fs.readFileSync(file).toString();
-  str = str.replace(pattern, replacement);
-  fs.writeFileSync(file, str);
-};
--- build/task/lint.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var sh = require('shelljs');
-
-module.exports = function () {
-  sh.exec('./node_modules/.bin/jshint build src test');
-  // Enable once ES6 support lands.
-  // sh.exec('./node_modules/.bin/jscs build src test');
-};
