30f9a2bc0bafccc3404b3070653bdc42b6ec9b45
Trey Shugart <treshugart@gmail.com>
2015-05-25 17:21:47 +1000

    document.createElement() -> src/api/create.js

--- test/unit/create-element.js
+++ test/unit/create-element.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import helpers from '../lib/helpers';
-import skate from '../../src/index';
+import apiCreate from '../../src/api/create';
 
 describe('create-element', function () {
   function resolved (element) {
@@ -11,27 +11,27 @@ describe('create-element', function () {
   it('If parentElementName is not provided, use elementName to lookup the definition in the registry.', function () {
     var { safe: tagName } = helpers.safeTagName();
     skate(tagName, {});
-    expect(resolved(document.createElement(tagName))).to.equal(true);
-    expect(resolved(document.createElement('div', tagName))).to.equal(false);
+    expect(resolved(apiCreate(tagName))).to.equal(true);
+    expect(resolved(apiCreate('div', tagName))).to.equal(false);
   });
 
   it('If parentElementName is provided, use it to lookup the definition in the registry.', function () {
     var { safe: tagName } = helpers.safeTagName();
     skate(tagName, { extends: 'div' });
-    expect(resolved(document.createElement(tagName))).to.equal(false);
-    expect(resolved(document.createElement('div', tagName))).to.equal(true);
+    expect(resolved(apiCreate(tagName))).to.equal(false);
+    expect(resolved(apiCreate('div', tagName))).to.equal(true);
   });
 
   it('If no registry entry is found, create the element using the saved method and return it.', function () {
     var { safe: tagName } = helpers.safeTagName();
-    expect(resolved(document.createElement(tagName))).to.equal(false);
-    expect(resolved(document.createElement('div', tagName))).to.equal(false);
+    expect(resolved(apiCreate(tagName))).to.equal(false);
+    expect(resolved(apiCreate('div', tagName))).to.equal(false);
   });
 
   it('If a registry entry matches parentElementName, and elementName does not match the value of the extends property, create the element from elementName and set the "is" attribute to parentElementName but do not initialise it.', function () {
     var { safe: tagName } = helpers.safeTagName();
     skate(tagName, { extends: 'div' });
-    var span = document.createElement('span', tagName);
+    var span = apiCreate('span', tagName);
     expect(span.getAttribute('is')).to.equal(tagName);
     expect(resolved(span)).to.equal(false);
   });
@@ -39,6 +39,6 @@ describe('create-element', function () {
   it('If a registry entry is found, use it to create the element and return it.', function () {
     var { safe: tagName } = helpers.safeTagName();
     skate(tagName, {});
-    expect(resolved(document.createElement(tagName))).to.equal(true);
+    expect(resolved(apiCreate(tagName))).to.equal(true);
   });
 });
