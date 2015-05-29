29aa042ae84d3b64c3876700c49882938fa2dbfc
Trey Shugart <treshugart@gmail.com>
2015-05-29 12:25:33 +1000

    Cleanup.

--- src/lifecycle/events.js
+++ src/lifecycle/events.js
@@ -1,3 +1,4 @@
+import chain from '../api/chain';
 import matchesSelector from '../util/matches-selector';
 
 function parseEvent (e) {
@@ -8,39 +9,40 @@ function parseEvent (e) {
   };
 }
 
-function makeHandler (elem, handler, delegate) {
+function makeDelegateHandler (elem, handler, delegate) {
   return function (e) {
-    // If we're not delegating, trigger directly on the component element.
-    if (!delegate) {
-      return handler(elem, e, elem);
-    }
-
-    // If we're delegating, but the elem doesn't match, then we've have
-    // to go up the tree until we find a matching ancestor or stop at the
-    // component element, or document. If a matching ancestor is found, the
-    // handler is triggered on it.
     var current = e.target;
-
     while (current && current !== document && current !== elem.parentNode) {
       if (matchesSelector(current, delegate)) {
         return handler(elem, e, current);
       }
-
       current = current.parentNode;
     }
   };
 }
 
+function makeNormalHandler (elem, handler) {
+  return function (e) {
+    handler(elem, e, elem);
+  };
+}
+
+function makeHandler (elem, handler, delegate) {
+  return delegate ?
+    makeDelegateHandler(elem, handler, delegate) :
+    makeNormalHandler(elem, handler);
+}
+
 export default function (elem, evts) {
   Object.keys(evts || {}).forEach(function (evt) {
-    var handlers = evts[evt];
+    var handler = chain(evts[evt]);
     var parsed = parseEvent(evt);
     var useCapture = !!parsed.delegate && (parsed.name === 'blur' || parsed.name === 'focus');
 
-    handlers = typeof handlers === 'function' ? [handlers] : handlers;
-    handlers.forEach(function (handler) {
-      handler = makeHandler(elem, handler, parsed.delegate);
-      elem.addEventListener(parsed.name, handler, useCapture);
-    });
+    elem.addEventListener(
+      parsed.name,
+      makeHandler(elem, handler, parsed.delegate),
+      useCapture
+    );
   });
 }
