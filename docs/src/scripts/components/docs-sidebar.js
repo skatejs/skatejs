cfc9af622a5de449f44829d7fb901f19630b2a5a
Trey Shugart <treshugart@gmail.com>
2015-06-23 00:08:42 +1000

    Docs progress.

--- docs/src/scripts/components/docs-sidebar.js
+++ docs/src/scripts/components/docs-sidebar.js
@@ -4,25 +4,58 @@ import skate from '../../../../src/index';
 export default skate('sk-docs-sidebar', {
   template: shade(`
     <sk-sidebar>
-      <sk-item><a href="/docs/">Getting Started</a></sk-item>
       <sk-item>
+        <a href="/docs/">Getting Started</a>
         <sk-item><a href="/docs/#h2-what-is-skate">What is Skate?</a></sk-item>
         <sk-item><a href="/docs/#h2-installing">Installing</a></sk-item>
         <sk-item><a href="/docs/#h2-native-vs-polyfill">Native vs Polyfill</a></sk-item>
         <sk-item><a href="/docs/#h2-element-lifecycle">Element Lifecycle</a></sk-item>
       </sk-item>
-      <sk-item><a href="/docs/skate.html">skate (name, options)</a></sk-item>
       <sk-item>
-        <sk-item><a href="/docs/options/attached.html">options.attached</a></sk-item>
-        <sk-item><a href="/docs/options/attribute.html">options.attribute</a></sk-item>
-        <sk-item><a href="/docs/options/created.html">options.created</a></sk-item>
-        <sk-item><a href="/docs/options/detached.html">options.detached</a></sk-item>
-        <sk-item><a href="/docs/options/events.html">options.events</a></sk-item>
-        <sk-item><a href="/docs/options/extends.html">options.extends</a></sk-item>
-        <sk-item><a href="/docs/options/properties.html">options.properties</a></sk-item>
-        <sk-item><a href="/docs/options/prototype.html">options.prototype</a></sk-item>
-        <sk-item><a href="/docs/options/template.html">options.template</a></sk-item>
-        <sk-item><a href="/docs/options/type.html">options.type</a></sk-item>
+        <a href="/docs/skate.html">API</a>
+        <sk-item>
+          <a href="/docs/skate.html">skate()</a>
+          <sk-item><a href="/docs/options/attached.html">attached</a></sk-item>
+          <sk-item><a href="/docs/options/attribute.html">attribute</a></sk-item>
+          <sk-item><a href="/docs/options/created.html">created</a></sk-item>
+          <sk-item><a href="/docs/options/detached.html">detached</a></sk-item>
+          <sk-item><a href="/docs/options/events.html">events</a></sk-item>
+          <sk-item><a href="/docs/options/extends.html">extends</a></sk-item>
+          <sk-item><a href="/docs/options/properties.html">properties</a></sk-item>
+          <sk-item><a href="/docs/options/prototype.html">prototype</a></sk-item>
+          <sk-item><a href="/docs/options/template.html">template</a></sk-item>
+          <sk-item><a href="/docs/options/type.html">type</a></sk-item>
+        </sk-item>
+        <sk-item>
+          <a href="/docs/skate/chain.html"><span class="subtle">skate.</span>chain()</a>
+        </sk-item>
+        <sk-item>
+          <a href="/docs/skate/create.html"><span class="subtle">skate.</span>create()</a>
+        </sk-item>
+        <sk-item>
+          <a href="/docs/skate/emit.html"><span class="subtle">skate.</span>emit()</a>
+        </sk-item>
+        <sk-item>
+          <a href="/docs/skate/init.html"><span class="subtle">skate.</span>init()</a>
+        </sk-item>
+        <sk-item>
+          <a href="/docs/skate/no-conflict.html"><span class="subtle">skate.</span>noConflict()</a>
+        </sk-item>
+        <sk-item>
+          <a href="/docs/skate/property.html"><span class="subtle">skate.</span>property()</a>
+        </sk-item>
+        <sk-item>
+          <a href="/docs/skate/ready.html"><span class="subtle">skate.</span>ready()</a>
+        </sk-item>
+        <sk-item>
+          <a href="/docs/skate/type.html"><span class="subtle">skate.</span>type</a>
+        </sk-item>
+        <sk-item>
+          <a href="/docs/skate/version.html"><span class="subtle">skate.</span>version</a>
+        </sk-item>
+        <sk-item>
+          <a href="/docs/skate/watch.html"><span class="subtle">skate.</span>watch()</a>
+        </sk-item>
       </sk-item>
     </sk-sidebar>
   `)
