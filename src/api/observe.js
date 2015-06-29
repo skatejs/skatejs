b18b741a42f8a1f30e155abda2d2d4da7c7ff55d
Trey Shugart <treshugart@gmail.com>
2015-06-29 10:36:38 +1000

    Check .init for undefined rather than falsy.
    Exploration on .observe().

--- src/api/observe.js
+++ src/api/observe.js
@@ -1,19 +1,69 @@
 import apiChain from './chain';
 import event from './event';
 import maybeThis from '../util/maybe-this';
-import parseEvent from '../util/parse-event';
+import utilData from '../util/data';
 
-var slice = Array.prototype.slice;
+function parseDependency (e) {
+  var path = e.split('.');
+  var name = path.pop();
+  return {
+    name: name,
+    path: path.join('.'),
+    paths: path,
+    pathname: e,
+    eventName: path.length ? `${name} *` : name
+  };
+}
 
 function observeOne (elem, handler, dependencies) {
-  var events;
+  var valueCache = {};
+
   handler = apiChain(handler);
-  events = dependencies.map(e => parseEvent(e));
-  events.forEach(function (e) {
-    event(elem, `skate.property.${e.name} ${e.selector}`, function () {
-      handler.apply(elem, events.map(function (e) {
-        return elem.querySelector(e.selector)[e.name];
-      }));
+  dependencies = dependencies.map(parseDependency);
+
+  dependencies.forEach(function (dependency) {
+    event(elem, `skate.property.${dependency.eventName}`, function (e) {
+      var target = e.target;
+
+      // Check up the tree to make sure all path descriptors match anscestors.
+      if (dependency.path) {
+        let paths = dependency.paths.slice(0);
+
+        // The target must be a component of the last part in the path.
+        if (!(paths[paths.length - 1] in utilData(target))) {
+          return;
+        }
+
+        // If the first one matches, it must be removed.
+        paths.pop();
+
+        // Start at the parent and match each ancestor against each path part.
+        let current = e.target.parentNode;
+        while (paths.length && current) {
+          if (paths[paths.length - 1] in utilData(current)) {
+            paths.pop();
+          }
+          current = current.parentNode;
+        }
+
+        // If there were parts that weren't matched, then the property wasn't
+        // triggered frome the correct point in the heirarchy.
+        if (paths.length) {
+          return;
+        }
+      }
+
+      // This will old the value references until the dependent property is
+      // updated. If the element is removed from the DOM, a reference to it
+      // will still exist here.
+      var changeRecord = e.detail;
+      changeRecord.target = e.target;
+      valueCache[dependency.pathname] = changeRecord;
+
+      // Only call the handler once all properties have been initialised.
+      if (Object.keys(valueCache).length === dependencies.length) {
+        handler.apply(elem, dependencies.map(d => valueCache[d.pathname]));
+      }
     });
   });
 }
