7018dbbbc4995ea36f5576adb4bf77fa086fcc9f
Trey Shugart <treshugart@gmail.com>
2015-07-15 17:24:45 +1000

    Remove ready util since it wasn't being used.

--- src/util/ready.js
+++
@@ -1,9 +0,0 @@
-export default function (fn) {
-  return function () {
-    if (document.readyState === 'complete') {
-      fn();
-    } else {
-      document.addEventListener('DOMContentLoaded', fn);
-    }
-  };
-}
