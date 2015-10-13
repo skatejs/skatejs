4410a1cbfa4b6019457ac03feed85f6643300769
Trey Shugart <treshugart@gmail.com>
2015-10-13 15:42:56 +1100

    Have to use slice() instead of ... because Array.from is es6 only.

--- src/api/fragment.js
+++ src/api/fragment.js
@@ -1,12 +1,14 @@
 import init from './init';
 import createFromHtml from '../util/create-from-html';
 
+const slice = Array.prototype.slice;
+
 function buildFragment (frag, arg) {
   if (arg) {
     if (typeof arg === 'string') {
-      arg = fragment(...createFromHtml(arg).childNodes);
+      arg = fragment.apply(null, slice.call(createFromHtml(arg).childNodes));
     } else if (arg.length) {
-      arg = fragment(...arg);
+      arg = fragment.apply(null, slice.call(arg));
     } else if (arg.nodeType) {
       init(arg);
     }
