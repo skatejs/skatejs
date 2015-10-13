0a70286b7e1300da0f956d20ebbb0d99aea02080
Trey Shugart <treshugart@gmail.com>
2015-10-13 11:09:28 +1100

    Make skate.render() work with all bound components.

--- src/api/render.js
+++ src/api/render.js
@@ -2,8 +2,5 @@ import registry from '../global/registry';
 import renderer from '../lifecycle/renderer';
 
 export default function (elem) {
-  const components = registry.find(elem);
-  if (components.length) {
-    renderer(elem, components[0]);
-  }
+  registry.find(elem).forEach(component => renderer(elem, component));
 }
