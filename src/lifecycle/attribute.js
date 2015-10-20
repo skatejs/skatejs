8544df2ef1a81e1f6a827892564aadd45d71bf05
Joscha Feth <jfeth@atlassian.com>
2015-10-20 17:17:33 +1100

    perf: early exit in case we did not get a callback

--- src/lifecycle/attribute.js
+++ src/lifecycle/attribute.js
@@ -1,5 +1,12 @@
+const noop = () => {};
+
 export default function (opts) {
-  let callback = opts.attribute || function () {};
+  let callback = opts.attribute;
+
+  if (typeof callback !== 'function') {
+    return noop;
+  }
+
   return function (name, oldValue, newValue) {
     callback(this, {
       name: name,
