dda5cf96e0a78928265d2ce8ad17fd6b0733954b
Trey Shugart <treshugart@gmail.com>
2015-11-07 18:10:10 +1100

    Update build.

--- dist/index.js
+++ dist/index.js
@@ -580,108 +580,46 @@
     exports: {}
   };
   var exports = module.exports;
-  var defineDependencies = {
-    "module": module,
-    "exports": exports
+  
+  /* eslint-disable no-unused-vars */
+  var hasOwnProperty = Object.prototype.hasOwnProperty;
+  var propIsEnumerable = Object.prototype.propertyIsEnumerable;
+  
+  function toObject(val) {
+  	if (val === null || val === undefined) {
+  		throw new TypeError('Object.assign cannot be called with null or undefined');
+  	}
+  
+  	return Object(val);
+  }
+  
+  module.exports = Object.assign || function (target, source) {
+  	var from;
+  	var to = toObject(target);
+  	var symbols;
+  
+  	for (var s = 1; s < arguments.length; s++) {
+  		from = Object(arguments[s]);
+  
+  		for (var key in from) {
+  			if (hasOwnProperty.call(from, key)) {
+  				to[key] = from[key];
+  			}
+  		}
+  
+  		if (Object.getOwnPropertySymbols) {
+  			symbols = Object.getOwnPropertySymbols(from);
+  			for (var i = 0; i < symbols.length; i++) {
+  				if (propIsEnumerable.call(from, symbols[i])) {
+  					to[symbols[i]] = from[symbols[i]];
+  				}
+  			}
+  		}
+  	}
+  
+  	return to;
   };
-  var define = function defineReplacementWrapper(generatedModuleName) {
-    return function defineReplacement(name, deps, func) {
-      var root = (typeof window === 'undefined' ? global : window);
-      var defineGlobal = root.define;
-      var rval;
-      var type;
   
-      func = [func, deps, name].filter(function (cur) {
-        return typeof cur === 'function';
-      })[0];
-      deps = [deps, name, []].filter(Array.isArray)[0];
-      rval = func.apply(null, deps.map(function (value) {
-        return defineDependencies[value];
-      }));
-      type = typeof rval;
-  
-      // Support existing AMD libs.
-      if (typeof defineGlobal === 'function') {
-        // Almond always expects a name so resolve one (#29).
-        defineGlobal(typeof name === 'string' ? name : generatedModuleName, deps, func);
-      }
-  
-      // Some processors like Babel don't check to make sure that the module value
-      // is not a primitive before calling Object.defineProperty() on it. We ensure
-      // it is an instance so that it can.
-      if (type === 'string') {
-        rval = String(rval);
-      } else if (type === 'number') {
-        rval = Number(rval);
-      } else if (type === 'boolean') {
-        rval = Boolean(rval);
-      }
-  
-      // Reset the exports to the defined module. This is how we convert AMD to
-      // CommonJS and ensures both can either co-exist, or be used separately. We
-      // only set it if it is not defined because there is no object representation
-      // of undefined, thus calling Object.defineProperty() on it would fail.
-      if (rval !== undefined) {
-        exports = module.exports = rval;
-      }
-    };
-  }("__bbda433df4ec72c4488e3a2f0e6a59a1");
-  define.amd = true;
-  
-  'use strict';
-  
-  (function (global, factory) {
-  	if (typeof define === 'function' && define.amd) {
-  		define(['exports'], factory);
-  	} else if (typeof exports !== 'undefined') {
-  		factory(exports);
-  	} else {
-  		var mod = {
-  			exports: {}
-  		};
-  		factory(mod.exports);
-  		global.index = mod.exports;
-  	}
-  })(undefined, function (exports) {
-  	/* eslint-disable no-unused-vars */
-  	var hasOwnProperty = Object.prototype.hasOwnProperty;
-  	var propIsEnumerable = Object.prototype.propertyIsEnumerable;
-  
-  	function toObject(val) {
-  		if (val === null || val === undefined) {
-  			throw new TypeError('Object.assign cannot be called with null or undefined');
-  		}
-  
-  		return Object(val);
-  	}
-  
-  	module.exports = Object.assign || function (target, source) {
-  		var from;
-  		var to = toObject(target);
-  		var symbols;
-  
-  		for (var s = 1; s < arguments.length; s++) {
-  			from = Object(arguments[s]);
-  
-  			for (var key in from) {
-  				if (hasOwnProperty.call(from, key)) {
-  					to[key] = from[key];
-  				}
-  			}
-  
-  			if (Object.getOwnPropertySymbols) {
-  				symbols = Object.getOwnPropertySymbols(from);
-  				for (var i = 0; i < symbols.length; i++) {
-  					if (propIsEnumerable.call(from, symbols[i])) {
-  						to[symbols[i]] = from[symbols[i]];
-  					}
-  				}
-  			}
-  		}
-  
-  		return to;
-  	};
-  });
   
   return module.exports;
 }).call(this);
