607354aa4e40f2fc4382257a98b4fb98c159e40b
Trey Shugart <treshugart@gmail.com>
2015-12-19 12:36:31 +1100

    Use real node so that there is a more comprehensive api to use instead of just the document fragment API (this also enablues use with jQuery).

--- src/api/properties/content.js
+++ src/api/properties/content.js
@@ -7,6 +7,7 @@ if (!MutationObserver) {
   throw new Error('Usage of the content property requires MutationObserver support.');
 }
 
+// Calls the `change` callback if it's defined.
 function change (el, cb) {
   cb = cb || function () {};
   return function (mo) {
@@ -14,60 +15,65 @@ function change (el, cb) {
   };
 }
 
-function createFakeProperty (node, opts) {
+// Creates a fake node for usage before the element is rendered so that the way
+// of accessing the value of the content node does not change at any point
+// during the rendering process. This is basically syntanctic sugar for not
+// having to do something like:
+//
+//     elem.content && elem.content.value
+//
+// In your `render` function. Instead, you can just do:
+//
+//     elem.content.value
+//
+// This is to get around having to know about the implementation details which
+// vary depending on if we're in native or polyfilled custom element land.
+function createFakeNode (name) {
   return { 
-    get [opts.nodeAccessor] () {
-      return node;
-    },
-    get [opts.valueAccessor] () {
+    get [name] () {
       return null;
     }
   };
 }
 
-function createRealProperty (node, opts) {
-  Object.defineProperties(node, {
-    [opts.nodeAccessor]: {
-      get () {
-        return node;
-      }
-    },
-    [opts.valueAccessor]: {
-      get () {
-        const ch = this.childNodes;
-        return ch && ch.length ? [].slice.call(ch) : null;
-      }
+// Creates a real node so that the renering process can attach nodes to it.
+function createRealNode (elem, name, selector) {
+  const node = selector ? elem.querySelector(selector) : document.createElement('div'); 
+  Object.defineProperty(node, name, {
+    get () {
+      const ch = this.childNodes;
+      return ch && ch.length ? [].slice.call(ch) : null;
     }
   });
   return node;
 }
 
-function init (el, nodes) {
+// Sets initial content for the specified node.
+function init (node, nodes) {
   for (let a = 0; a < nodes.length; a++) {
-    el.appendChild(nodes[a]);
+    node.appendChild(nodes[a]);
   }
 }
 
 export default function (opts = {}) {
   opts = assign({
-    nodeAccessor: 'node',
-    valueAccessor: 'nodes',
+    accessor: 'nodes',
     change: function () {},
-    type: 'div'
+    selector: ''
   }, opts);
   return {
     created (el) {
       const info = data(el);
-      info.contentNode = createFakeProperty(document.createElement(opts.type), opts);
+      info.contentNode = createFakeNode(opts.accessor);
       info.initialState = [].slice.call(el.childNodes);
     },
     get (el) {
       return data(el).contentNode;
     },
-    ready (el) {
-      const info = data(el);
-      const observer = new MutationObserver(change(el, opts.change));
-      info.contentNode = createRealProperty(info.contentNode.node, opts);
+    ready (elem) {
+      const info = data(elem);
+      const observer = new MutationObserver(change(elem, opts.change));
+      info.contentNode = createRealNode(elem, opts.accessor, opts.selector);
       init(info.contentNode, info.initialState);
       observer.observe(info.contentNode, { childList: true });
     }
