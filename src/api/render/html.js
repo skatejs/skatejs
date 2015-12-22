a1a97883bab2f7f4765a27890721dfc39a59f419
Trey Shugart <treshugart@gmail.com>
2015-12-22 15:03:06 +1100

    Fixes #374 - Fix skate.render.html() so that innerHTML is available in the function.

--- src/api/render/html.js
+++ src/api/render/html.js
@@ -2,9 +2,10 @@ import fragment from '../fragment';
 
 export default function (render) {
   return function (elem) {
+    const rendered = render(elem);
     while (elem.childNodes.length) {
       elem.removeChild(elem.childNodes[0]);
     }
-    elem.appendChild(fragment(render(elem)));
+    elem.appendChild(fragment(rendered));
   };
 }
