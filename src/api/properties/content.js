676ef05b5cdd6b92e00e726a6e0debc1d13e8be6
Trey Shugart <treshugart@gmail.com>
2015-12-22 22:32:42 +1100

    Fix bad values from being passed into fragment(). Since fragment() is now fixed, we don't have to check data in the content prop.

--- src/api/properties/content.js
+++ src/api/properties/content.js
@@ -1,3 +1,68 @@
+import init from './init';
+
+const { Node, NodeList } = window;
+const slice = Array.prototype.slice;
+const specialMap = {
+  caption: 'table',
+  dd: 'dl',
+  dt: 'dl',
+  li: 'ul',
+  tbody: 'table',
+  td: 'tr',
+  thead: 'table',
+  tr: 'tbody'
+};
+
+function resolveParent (tag, html) {
+  const container = document.createElement('div');
+  let levels = 0;
+  let parentTag = specialMap[tag];
+
+  while (parentTag) {
+    html = `<${parentTag}>${html}</${parentTag}>`;
+    ++levels;
+    parentTag = specialMap[parentTag];
+  }
+
+  container.innerHTML = html;
+
+  let parent = container;
+  for (let a = 0; a < levels; a++) {
+    parent = parent.firstElementChild;
+  }
+  return parent;
+}
+
+function resolveTag (html) {
+  const tag = html.match(/^<([^\s>]+)/);
+  return tag && tag[1];
+}
+
+function resolveHtml (html) {
+  return resolveParent(resolveTag(html), html);
+}
+
+export default function fragment (...args) {
+  return args.reduce(function (frag, node) {
+    if (typeof node === 'string') {
+      node = fragment.apply(null, slice.call(resolveHtml(node).childNodes));
+    } else if (node instanceof NodeList || Array.isArray(node)) {
+      node = fragment.apply(null, slice.call(node));
+    } else if (node instanceof Node) {
+      init(node);
+    }
+
+    if (node instanceof Node) {
+      frag.appendChild(node);
+    }
+
+    return frag;
+  }, document.createDocumentFragment());
+}
+
+
+
+
 import data from '../../util/data';
 import fragment from '../fragment';
 
@@ -66,10 +131,7 @@ export default {
   },
   set (elem, change) {
     const eldata = data(elem);
-    
-    // If the initial state hasn't been set yet then we can't do anything.
-    if (!eldata.contentPropertyInitialState) {
-      eldata.contentProperty.appendChild(fragment(change.newValue));
-    }
+    eldata.contentProperty.appendChild(fragment(change.newValue));
   }
 };
+
