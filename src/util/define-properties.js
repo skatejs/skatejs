0d56522d9ae59d9d62c6787421cfcc4daf489a92
Trey Shugart <treshugart@gmail.com>
2016-01-06 22:42:44 +1100

    Make defining properties more robust for opera 12 or any browser that thinks things are non-configurable.

--- src/util/define-properties.js
+++ src/util/define-properties.js
@@ -1,8 +1,14 @@
 export default function (obj, props) {
   Object.keys(props).forEach(function (name) {
-    const desc = Object.getOwnPropertyDescriptor(obj, name);
-    if (!desc || desc.configurable) {
-      Object.defineProperty(obj, name, props[name]);
+    const prop = props[name];
+    const descrptor = Object.getOwnPropertyDescriptor(obj, name);
+    const isDinosaurBrowser = name !== 'arguments' && name !== 'caller' && 'value' in prop;
+    const isConfigurable = !descrptor || descrptor.configurable;
+
+    if (isConfigurable) {
+      Object.defineProperty(obj, name, prop);
+    } else if (isDinosaurBrowser) {
+      obj[name] = prop.value;
     }
   });
 }
