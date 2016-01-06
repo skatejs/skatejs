82aa0e0d0990d25535a75ae9485d6861208d9a6d
Trey Shugart <treshugart@gmail.com>
2016-01-06 17:30:28 +1100

    Fix safari issues with mocha's expect.to.throw api.

--- test/unit/document-observer.js
+++ test/unit/document-observer.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import globals from '../../src/global/vars';
 import observer from '../../src/global/document-observer';
 
@@ -18,7 +16,6 @@ describe('Document Observer', function () {
       disconnect: function () {},
       takeRecords: function () {}
     };
-
     return getObserver();
   }
 
@@ -38,8 +35,7 @@ describe('Document Observer', function () {
   });
 
   it('should not overwrite an existing observer', function () {
-    var oldObserver = mockObserver();
-
+    const oldObserver = mockObserver();
     observer.register();
     expect(getObserver()).to.equal(oldObserver);
   });
@@ -47,7 +43,11 @@ describe('Document Observer', function () {
   it('should throw an error if a MutationObserver implementation isn\'t found', function () {
     const oldMutationObserver = window.MutationObserver;
     window.MutationObserver = undefined;
-    expect(observer.register).to.throw('Mutation Observers are not supported by this browser. Skate requires them in order to polyfill the behaviour of Custom Elements. If you want to support this browser you should include a Mutation Observer polyfill before Skate.');
+    try {
+      observer.register();
+    } catch (e) {
+      expect(e.message).to.equal('Mutation Observers are not supported by this browser. Skate requires them in order to polyfill the behaviour of Custom Elements. If you want to support this browser you should include a Mutation Observer polyfill before Skate.');
+    }
     window.MutationObserver = oldMutationObserver;
   });
 });
