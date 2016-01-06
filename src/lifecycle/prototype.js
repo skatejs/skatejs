ac26a117537b729c17fa3cebc16105963646bbfe
Trey Shugart <treshugart@gmail.com>
2016-01-06 17:47:35 +1100

    Update the lifecycle prototype setter and remove usage of assign-safe as it's not used anymore.

--- src/lifecycle/prototype.js
+++ src/lifecycle/prototype.js
@@ -1,12 +1,13 @@
-import assignSafe from '../util/assign-safe';
 import protos from '../util/protos';
+import utilDefineProperties from '../util/define-properties';
+import utilGetOwnPropertyDescriptors from '../util/get-own-property-descriptors';
 
 export default function prototype (opts) {
   let prototypes = protos(opts.prototype);
   return function (elem) {
     prototypes.forEach(function (proto) {
       if (!proto.isPrototypeOf(elem)) {
-        assignSafe(elem, proto);
+        utilDefineProperties(elem, utilGetOwnPropertyDescriptors(proto));
       }
     });
   };
