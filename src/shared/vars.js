cd4a8b281df60e646e5253dd50ed94e795787fd9
Trey Shugart <treshugart@gmail.com>
2016-03-05 09:48:09 +1100

    Add comment about shared vars version.

--- src/shared/vars.js
+++ src/shared/vars.js
@@ -1,3 +1,7 @@
+// This should only be changed / incremented when the internal shared API
+// changes in a backward incompatible way. It's designed to be shared with
+// other Skate versions if it's compatible for performance reasons and also
+// to align with the spec since in native there is a global registry.
 const VERSION = '__skate_0_16_0';
 
 if (!window[VERSION]) {
