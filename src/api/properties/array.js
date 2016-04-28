813fede79c31161c54edea2fd706de79ba9c9769
Trey Shugart <treshugart@gmail.com>
2016-04-28 13:08:25 +1000

    Make code a bit more concise.

--- src/api/properties/array.js
+++ src/api/properties/array.js
@@ -1,16 +1,6 @@
-const { parse, stringify } = window.JSON;
-
 export default {
-  coerce (val) {
-    return Array.isArray(val) ? val : [val];
-  },
-  default () {
-    return [];
-  },
-  deserialize (val) {
-    return parse(val);
-  },
-  serialize (val) {
-    return stringify(val);
-  }
+  coerce: val => Array.isArray(val) ? val : [val],
+  default: () => [],
+  deserialize: JSON.parse,
+  serialize: JSON.stringify
 };
