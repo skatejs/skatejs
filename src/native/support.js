25d0eb83d4e0c5842e7274f35774372c9e0b5da6
Trey Shugart <treshugart@gmail.com>
2016-05-06 22:29:41 +1000

    Fix polyfilled flag.

--- src/native/support.js
+++ src/native/support.js
@@ -2,6 +2,6 @@ import registerElement from './register-element';
 
 const v0 = !!registerElement;
 const v1 = !!window.customElements;
-const polyfilled = !v0 || !v1;
+const polyfilled = !v0 && !v1;
 
 export default { v0, v1, polyfilled };
