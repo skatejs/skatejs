34ec70d69f7c3b41e830205bbe92f27fb5514ea5
Trey Shugart <treshugart@gmail.com>
2016-05-10 12:45:59 +1000

    Update debounce() to use requestAnimationFrame() if available.

--- src/util/debounce.js
+++ src/util/debounce.js
@@ -1,13 +1,14 @@
+const raf = requestAnimationFrame || setTimeout;
 export default function (fn) {
   var called = false;
 
   return function (...args) {
     if (!called) {
       called = true;
-      setTimeout(() => {
+      raf(() => {
         called = false;
         fn.apply(this, args);
-      }, 1);
+      });
     }
   };
 }
