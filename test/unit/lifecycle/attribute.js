cfb43984ac2301003cac0c3038f633b0a7f29809
Trey Shugart <treshugart@gmail.com>
2016-05-16 21:06:14 +1000

    Write tests for attribute invocation and consilidate attribute lifecycle tests.

--- test/unit/lifecycle/attribute.js
+++ test/unit/lifecycle/attribute.js
@@ -1,22 +1,42 @@
-import helperElement from '../lib/element';
+import element from '../../lib/element';
+import fixture from '../../lib/fixture';
 import ready from '../../src/api/ready';
 import skate from '../../src/index';
 
-describe('lifecycle/attribute', function () {
+describe('lifecycle/attribute', function (done) {
+  it('should be invoked for attributes that exist on the element when it is created', function (done) {
+    const tag = element();
+    const elem = document.createElement(tag.safe);
+    fixture(elem);
+    elem.setAttribute('test', 'testing');
+    expect(elem.hasAttribute('defined')).to.equal(false);
+    tag.skate({
+      attribute (elem, data) {
+        if (data.name === 'defined') {
+          return;
+        }
+        expect(data.name).to.equal('test');
+        expect(data.oldValue).to.equal(undefined);
+        expect(data.newValue).to.equal('testing');
+        done();
+      }
+    });
+  });
+
   describe('Callback', function () {
     let myToggle;
     let spy;
-    beforeEach(() => {
-      let tag = helperElement('test-toggle');
+
+    beforeEach(function () {
       spy = sinon.spy();
-      let MyToggle = skate(tag.safe, {
+      let MyToggle = element().skate({
         attribute: spy
       });
       myToggle = new MyToggle();
     });
 
     it('should properly call the attribute callback for defined', function(done) {
-      let resolvedSpy = spy.withArgs(myToggle, sinon.match({name: 'defined', newValue: ''}));
+      let resolvedSpy = spy.withArgs(myToggle, sinon.match({name: 'defined', newValue: '' }));
 
       ready(myToggle, function() {
         expect(resolvedSpy.calledOnce).to.be.true;
@@ -46,7 +66,7 @@ describe('lifecycle/attribute', function () {
   describe('Attributes added via HTML', function () {
     it('should ensure the callback is called for attributes already on the element when it is initialised', function () {
       var called = false;
-      var tag = helperElement();
+      var tag = element();
 
       skate(tag.safe, {
         attribute: () => called = true
