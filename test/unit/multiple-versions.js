7ba143ef8485f6dafa39bb3c0b0d0542ed3da672
Trey Shugart <treshugart@gmail.com>
2016-05-17 10:53:22 +1000

    Uncomment commented out test.

--- test/unit/multiple-versions.js
+++ test/unit/multiple-versions.js
@@ -1,32 +1,32 @@
-// import helperElement from '../lib/element';
-// import helperFixture from '../lib/fixture';
+import helperElement from '../lib/element';
+import helperFixture from '../lib/fixture';
 
-// import skateMaster from '../../src/index';
-// import '../skate/0.14.3.js';
+import skateMaster from '../../src/index';
+import '../skate/0.14.3.js';
 
-// const { skate } = window;
+const { skate } = window;
 
-// describe('multiple-versions', function () {
-//   it('is possible to have multiple versions of skate on the page', function (done) {
-//     let called = [];
+describe('multiple-versions', function () {
+  it('is possible to have multiple versions of skate on the page', function (done) {
+    let called = [];
 
-//     function skateAndCreate(customSkate) {
-//       const el = helperElement();
-//       customSkate(el.safe, {
-//         created () {
-//           called.push(customSkate.version);
-//         }
-//       });
-//       let elem = document.createElement(el.safe);
-//       helperFixture(elem);
-//     }
+    function skateAndCreate(customSkate) {
+      const el = helperElement();
+      customSkate(el.safe, {
+        created () {
+          called.push(customSkate.version);
+        }
+      });
+      let elem = document.createElement(el.safe);
+      helperFixture(elem);
+    }
 
-//     skateAndCreate(skateMaster);
-//     skateAndCreate(skate);
+    skateAndCreate(skateMaster);
+    skateAndCreate(skate);
 
-//     setTimeout(function () {
-//       expect(called.sort()).to.deep.equal([skateMaster.version, skate.version].sort());
-//       done();
-//     }, 1);
-//   });
-// });
+    setTimeout(function () {
+      expect(called.sort()).to.deep.equal([skateMaster.version, skate.version].sort());
+      done();
+    }, 1);
+  });
+});
