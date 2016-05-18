d035817ca7a4de9fd97db91dd3b96055174e9e87
Trey Shugart <treshugart@gmail.com>
2016-05-19 09:56:25 +1000

    Try and use multiple karma configs so we can get the mutation observer polyfill on the test page.

--- karma.conf.js
+++ karma.conf.js
@@ -1,1 +1,9 @@
-module.exports = require('skatejs-build/karma.conf');
+const base = require('skatejs-build/karma.conf');
+module.exports = function (config) {
+  base(config);
+  config.set({
+    files: [
+      'node_modules/webcomponentsjs/MutationObserver.js'
+    ]
+  });
+};
