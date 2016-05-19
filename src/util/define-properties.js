ba3ff79946cc25f8b3f5eebe3f544d0c8c233dfa
Trey Shugart <treshugart@gmail.com>
2016-05-19 12:10:47 +1000

    Fix variable spelling error and ensure prop is writable before applying it.

--- src/util/define-properties.js
+++ src/util/define-properties.js
@@ -1,13 +1,14 @@
 export default function (obj, props) {
   Object.keys(props).forEach(function (name) {
     const prop = props[name];
-    const descrptor = Object.getOwnPropertyDescriptor(obj, name);
+    const descriptor = Object.getOwnPropertyDescriptor(obj, name);
     const isDinosaurBrowser = name !== 'arguments' && name !== 'caller' && 'value' in prop;
-    const isConfigurable = !descrptor || descrptor.configurable;
+    const isConfigurable = !descriptor || descriptor.configurable;
+    const isWritable = !descriptor || descriptor.writable;
 
     if (isConfigurable) {
       Object.defineProperty(obj, name, prop);
-    } else if (isDinosaurBrowser) {
+    } else if (isDinosaurBrowser && isWritable) {
       obj[name] = prop.value;
     }
   });
