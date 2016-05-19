54fb81856df5e335303eb6847f80ba5ab74f038c
Trey Shugart <treshugart@gmail.com>
2016-05-19 17:44:11 +1000

    Re-enable mutation observer in tests for older browsers.

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,8 @@ const base = require('skatejs-build/karma.conf');
 module.exports = function (config) {
   base(config);
   config.files = [
-    'node_modules/skatejs-named-slots/dist/index.js'
+    'node_modules/skatejs-named-slots/dist/index.js',
+    'node_modules/webcomponents.js/MutationObserver.js'
     // 'node_modules/webcomponents.js/webcomponents.js'
   ].concat(config.files);
 };
