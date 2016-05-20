f8c372673f5f3c1212e68069fa44396e37b53e43
Trey Shugart <treshugart@gmail.com>
2016-05-20 16:01:12 +1000

    Run tests without polyfills as we want to get a base browser support. When adding polyfills you are then subject to their version range.

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,5 @@
 const base = require('skatejs-build/karma.conf');
 module.exports = function (config) {
   base(config);
-  config.files = (function () {
-    if (process.argv.indexOf('--webcomponentsjs') > -1) {
-      return ['node_modules/webcomponents.js/webcomponents.js'];
-    } else if (process.argv.indexOf('--named-slots') > -1) {
-      return ['node_modules/skatejs-named-slots/dist/index.js', 'node_modules/webcomponents.js/MutationObserver.js'];
-    }
-    return ['node_modules/webcomponents.js/MutationObserver.js'];
-  }()).concat(config.files);
+  config.files = ['node_modules/webcomponents.js/MutationObserver.js'].concat(config.files);
 };
