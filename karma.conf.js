d2444e43b30bc9f5b0ad64b5aa1c797c17107119
Trey Shugart <treshugart@gmail.com>
2016-05-20 12:27:09 +1000

    Add different test permutations to integrate with different polyfills.

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,12 @@
 const base = require('skatejs-build/karma.conf');
 module.exports = function (config) {
   base(config);
-  config.files = [
-    'node_modules/skatejs-named-slots/dist/index.js',
-    'node_modules/webcomponents.js/MutationObserver.js'
-    // 'node_modules/webcomponents.js/webcomponents.js'
-  ].concat(config.files);
+  config.files = (function () {
+    if (process.argv.indexOf('--webcomponentsjs') > -1) {
+      return ['node_modules/webcomponents.js/webcomponents.js'];
+    } else if (process.argv.indexOf('--named-slots') > -1) {
+      return ['node_modules/skatejs-named-slots/dist/index.js', 'node_modules/webcomponents.js/MutationObserver.js'];
+    }
+    return ['node_modules/webcomponents.js/MutationObserver.js'];
+  }()).concat(config.files);
 };
