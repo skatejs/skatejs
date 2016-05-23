dfd1dbd6541e5dc0feb4a858a37272bb568d17e9
Trey Shugart <treshugart@gmail.com>
2016-05-23 11:54:13 +1000

    Up the no activity timeout to try and get iOS passing.

--- karma.conf.js
+++ karma.conf.js
@@ -2,4 +2,5 @@ const base = require('skatejs-build/karma.conf');
 module.exports = function (config) {
   base(config);
   config.files = ['node_modules/webcomponents.js/MutationObserver.js'].concat(config.files);
+  config.browserNoActivityTimeout = 60000;
 };
