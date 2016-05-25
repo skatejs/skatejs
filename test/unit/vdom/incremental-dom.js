565fda57879c693dc957900e380f65dfc36027f2
Trey Shugart <treshugart@gmail.com>
2016-05-25 13:53:58 +1000

    chore: Update Incremental DOM.

--- test/unit/vdom/incremental-dom.js
+++ test/unit/vdom/incremental-dom.js
@@ -1,5 +1,5 @@
 import { IncrementalDOM as VdomIncrementalDOM } from '../../../src/api/vdom';
-import IncrementalDOM from 'incremental-dom';
+import * as IncrementalDOM from 'incremental-dom';
 
 describe('IncrementalDOM', function () {
   it('should export all the same members as the incremental-dom we consume', function () {
