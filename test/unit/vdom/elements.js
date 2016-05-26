6f628369a1d2aa78590df63116f3d132fd2dc993
Trey Shugart <treshugart@gmail.com>
2016-05-26 12:11:16 +1000

    Better testing of support scenarios.

--- test/unit/vdom/elements.js
+++ test/unit/vdom/elements.js
@@ -2,8 +2,6 @@ import element from '../../lib/element';
 import support from '../../../src/native/support';
 import { symbols, vdom } from '../../../src/index';
 
-const shadowDomV0 = !support.shadowDomV1 && support.shadowDomV0;
-
 describe('vdom/elements', function () {
   it('slot', function () {
     const elem1 = element().skate({
@@ -17,19 +15,25 @@ describe('vdom/elements', function () {
       }
     })();
 
+    function assertSlotElement () {
+      expect(ch1.tagName).to.equal('SLOT', 'vdom');
+      expect(ch1.getAttribute('name')).to.equal('test', 'vdom');
+      expect(ch2.tagName).to.equal('SLOT', 'vdom(slot)');
+      expect(ch2.getAttribute('name')).to.equal('test', 'vdom(slot)');
+    }
+
     const ch1 = elem1[symbols.shadowRoot].firstElementChild;
     const ch2 = elem2[symbols.shadowRoot].firstElementChild;
 
-    if (shadowDomV0) {
+    if (support.shadowDomV1) {
+      assertSlotElement();
+    } else if (support.shadowDomV0) {
       expect(ch1.tagName).to.equal('CONTENT', 'vdom');
       expect(ch1.getAttribute('select')).to.equal('[slot="test"]', 'vdom');
       expect(ch2.tagName).to.equal('CONTENT', 'vdom(slot)');
       expect(ch2.getAttribute('select')).to.equal('[slot="test"]', 'vdom(slot)');
     } else {
-      expect(ch1.tagName).to.equal('SLOT', 'vdom');
-      expect(ch1.getAttribute('name')).to.equal('test', 'vdom');
-      expect(ch2.tagName).to.equal('SLOT', 'vdom(slot)');
-      expect(ch2.getAttribute('name')).to.equal('test', 'vdom(slot)');
+      assertSlotElement();
     }
   });
 });
