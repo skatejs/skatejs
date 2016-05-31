b4658eb44175536c5e315c5708a5c2b24d262766
Trey Shugart <treshugart@gmail.com>
2016-05-31 20:43:12 +1000

    Simplify emit().

--- src/api/emit.js
+++ src/api/emit.js
@@ -1,5 +1,3 @@
-import utilElementContains from '../util/element-contains';
-
 const CustomEvent = (function (CustomEvent) {
   if (CustomEvent) {
     try {
@@ -15,76 +13,14 @@ function createCustomEvent (name, opts = {}) {
   if (CustomEvent) {
     return new CustomEvent(name, opts);
   }
-
-  var e = document.createEvent('CustomEvent');
+  const e = document.createEvent('CustomEvent');
   e.initCustomEvent(name, opts.bubbles, opts.cancelable, opts.detail);
   return e;
 }
 
-function dispatch (elem, cEvent) {
-  if (!elem.disabled) {
-    return elem.dispatchEvent(cEvent);
-  }
-  cEvent.isPropagationStopped = true;
-}
-
-const hasBubbleOnDetachedElements = (function () {
-  var parent = document.createElement('div');
-  var child = document.createElement('div');
-  var hasBubbleOnDetachedElements = false;
-  parent.appendChild(child);
-  parent.addEventListener('test', () => hasBubbleOnDetachedElements = true);
-  child.dispatchEvent(createCustomEvent('test', { bubbles: true }));
-  return hasBubbleOnDetachedElements;
-}());
-
-function createReadableStopPropagation (oldStopPropagation) {
-  return function () {
-    this.isPropagationStopped = true;
-    oldStopPropagation.call(this);
-  };
-}
-
-function simulateBubbling (elem, cEvent) {
-  let didPreventDefault;
-  let currentElem = elem;
-  cEvent.stopPropagation = createReadableStopPropagation(cEvent.stopPropagation);
-  Object.defineProperty(cEvent, 'target', { get: () => elem });
-  while (currentElem && !cEvent.isPropagationStopped) {
-    Object.defineProperty(cEvent, 'currentTarget', {
-      configurable: true,
-      get () { return currentElem; }
-    });
-    if (dispatch(currentElem, cEvent) === false) {
-      didPreventDefault = false;
-    }
-    currentElem = currentElem.parentNode;
-  }
-  return didPreventDefault;
-}
-
-function emitOne (elem, name, opts) {
-  var cEvent, shouldSimulateBubbling;
-
+export default function (elem, name, opts = {}) {
   /* jshint expr: true */
   opts.bubbles === undefined && (opts.bubbles = true);
   opts.cancelable === undefined && (opts.cancelable = true);
-  cEvent = createCustomEvent(name, opts);
-  shouldSimulateBubbling = opts.bubbles &&
-    !hasBubbleOnDetachedElements &&
-    !utilElementContains(document, elem);
-
-  return shouldSimulateBubbling ?
-    simulateBubbling(elem, cEvent) :
-    dispatch(elem, cEvent);
-}
-
-export default function (elem, name, opts = {}) {
-  var names = (typeof name === 'string' ? name.split(' ') : name);
-  return names.reduce(function (prev, curr) {
-    if (emitOne(elem, curr, opts) === false) {
-      prev.push(curr);
-    }
-    return prev;
-  }, []);
+  return elem.disabled ? true : elem.dispatchEvent(createCustomEvent(name, opts));
 }
