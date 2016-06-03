86a365192aa3f0322a9365c4568947326a108fa1
Trey Shugart <treshugart@gmail.com>
2016-06-03 16:47:59 +1000

    Remove all unused code.

--- src/util/protos.js
+++
@@ -1,9 +0,0 @@
-export default function (proto) {
-  var chains = [];
-  while (proto) {
-    chains.push(proto);
-    proto = Object.getPrototypeOf(proto);
-  }
-  chains.reverse();
-  return chains;
-}
