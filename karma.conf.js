8d8bf3afc42773a69f322f36bb81846201c10008
Trey Shugart <treshugart@gmail.com>
2016-06-06 11:35:30 +1000

    Consume v1 polyfill from npm.

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 const base = require('skatejs-build/karma.conf');
 module.exports = function (config) {
   base(config);
-  config.files = ['node_modules/webcomponents.js/MutationObserver.js'].concat(config.files);
+  config.files = [
+    'node_modules/webcomponents.js/MutationObserver.js'
+  ].concat(config.files);
   config.browserNoActivityTimeout = 60000;
 };
