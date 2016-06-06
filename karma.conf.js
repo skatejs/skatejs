8de95a7d4680744cd580c22a733e7f7bf4c816d0
Trey Shugart <treshugart@gmail.com>
2016-06-06 14:16:37 +1000

    Add es6 shims for testing with the web componentsjs polyfill. Update to latest fix for webcomponentjs custom element v1 polyfill.

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,7 @@ const base = require('skatejs-build/karma.conf');
 module.exports = function (config) {
   base(config);
   config.files = [
+    'node_modules/es6-shim/es6-shim.js',
     'node_modules/webcomponents.js/MutationObserver.js'
   ].concat(config.files);
   config.browserNoActivityTimeout = 60000;
