53a9edf0422779a1f491a4c99a89cf7067552e8b
Trey Shugart <treshugart@gmail.com>
2016-06-07 10:32:57 +1000

    Adjust config to only run in latest IE (11).

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,18 @@
 const base = require('skatejs-build/karma.conf');
 module.exports = function (config) {
   base(config);
+
+  // Remove all explicit IE definitions.
+  config.browsers = config.browsers.filter(name => !/^internet_explorer/.test(name));
+
+  // Only test IE latest.
+  config.browsers.push('internet_explorer_latest');
+
+  // Shims for testing.
   config.files = [
     'node_modules/es6-shim/es6-shim.js'
   ].concat(config.files);
+
+  // Ensure mobile browsers have enough time to run.
   config.browserNoActivityTimeout = 60000;
 };
