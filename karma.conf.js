827eb5533a6e7f967a40f82871647902dc6b833b
Trey Shugart <treshugart@gmail.com>
2016-06-30 21:11:01 +1000

    chore(test): Fix tests so that it only tries to run IE11 if running in Sauce Labs.
    For some reason it just started happening that it would try to run IE11 outside of Sauce Labs.

--- karma.conf.js
+++ karma.conf.js
@@ -2,15 +2,18 @@ const base = require('skatejs-build/karma.conf');
 module.exports = function (config) {
   base(config);
 
-  // Remove all explicit IE definitions.
-  config.browsers = config.browsers.filter(name => !/^internet_explorer/.test(name));
+  // Setup IE if testing in SauceLabs.
+  if (config.sauceLabs) {
+    // Remove all explicit IE definitions.
+    config.browsers = config.browsers.filter(name => !/^internet_explorer/.test(name));
 
-  // Only test IE latest.
-  config.browsers.push('internet_explorer_11');
+    // Only test IE latest.
+    config.browsers.push('internet_explorer_11');
+  }
 
   // Shims for testing.
   config.files = [
-    'node_modules/es6-shim/es6-shim.js'
+    'node_modules/es6-shim/es6-shim.js',
   ].concat(config.files);
 
   // Ensure mobile browsers have enough time to run.
