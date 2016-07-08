5a52fa012b630171707891e11daa3bfa5a82b16d
Trey Shugart <treshugart@gmail.com>
2016-07-08 13:15:38 +1000

    fix(support): Fix issue causing the v0 custom element HTMLElement override to be used even in v1 env
    #652

--- src/util/support.js
+++ src/util/support.js
@@ -1,6 +1,6 @@
 const div = document.createElement('div');
 export const customElementsV0 = !!document.registerElement;
-export const customElementsV0Polyfill = !Document.prototype.registerElement;
+export const customElementsV0Polyfill = customElementsV0 && !Document.prototype.registerElement;
 export const customElementsV1 = !!window.customElements;
 export const shadowDomV0 = !!div.createShadowRoot;
 export const shadowDomV1 = !!div.attachShadow;
