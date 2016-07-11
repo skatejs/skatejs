c75aa0520bf54670bddb7000505a47a5dfc620b7
Trey Shugart <treshugart@gmail.com>
2016-07-11 21:41:35 +1000

    refactor(vdom): Refactored tests to reuse more code.
    #646

--- test/unit/vdom/incremental-dom.js
+++ test/unit/vdom/incremental-dom.js
@@ -2,36 +2,18 @@ import * as IncrementalDOM from 'incremental-dom';
 import { vdom } from '../../../src/index';
 
 function testBasicApi (name) {
-  it('should be a function', () => expect(vdom[name]).to.be.a('function'));
-  it('should not be the same one as in Incremental DOM', () => expect(vdom[name]).not.to.equal(IncrementalDOM[name]));
+  describe(name, () => {
+    it('should be a function', () => expect(vdom[name]).to.be.a('function'));
+    it('should not be the same one as in Incremental DOM', () => expect(vdom[name]).not.to.equal(IncrementalDOM[name]));
+  });
 }
 
 describe('IncrementalDOM', function () {
-  describe('attr', () => {
-    testBasicApi('attr');
-  });
-
-  describe('elementClose', () => {
-    testBasicApi('elementClose');
-  });
-
-  describe('elementOpen', () => {
-    testBasicApi('elementOpen');
-  });
-
-  describe('elementOpenEnd', () => {
-    testBasicApi('elementOpenEnd');
-  });
-
-  describe('elementOpenStart', () => {
-    testBasicApi('elementOpenStart');
-  });
-
-  describe('elementVoid', () => {
-    testBasicApi('elementVoid');
-  });
-
-  describe('text', () => {
-    testBasicApi('text');
-  });
+  testBasicApi('attr');
+  testBasicApi('elementClose');
+  testBasicApi('elementOpen');
+  testBasicApi('elementOpenEnd');
+  testBasicApi('elementOpenStart');
+  testBasicApi('elementVoid');
+  testBasicApi('text');
 });
