9c4c8c315314833e0ca3f27f29ba407a0bbeeafb
Trey Shugart <treshugart@gmail.com>
2016-07-14 09:56:28 +1000

    test(ref): Split out ref tests and write test to ensure ref callback has access to descendants.
    #666

--- test/unit/vdom/ref.js
+++ test/unit/vdom/ref.js
@@ -1,19 +1,31 @@
 import { define, vdom } from '../../../src/index';
 
-describe.only('vdom/ref', () => {
+describe('vdom/ref', () => {
   function create(ref) {
     const Elem = define('x-test', {
       render() {
-        vdom.element('div', { ref, id: 'test' });
+        vdom.element('div', { ref, id: 'div' }, () => vdom.element('span', { id: 'span' }, 'test'));
       }
     });
     return new Elem();
   }
 
-  it('can be a function', done => {
+  it('should be a function', done => {
+    create(() => done());
+  });
+
+  it('should have access to all properties', done => {
     create(node => {
-      expect(node.tagName).to.equal('DIV');
-      expect(node.id).to.equal('test');
+      expect(node.id).to.equal('div');
+      done();
+    });
+  });
+
+  it('should have access to all descendants', done => {
+    create(node => {
+      expect(node.firstChild.tagName).to.equal('SPAN');
+      expect(node.firstChild.id).to.equal('span');
+      expect(node.firstChild.textContent).to.equal('test');
       done();
     });
   });
