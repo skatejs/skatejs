23b3f33490c3ada14bb08c8fce86d2003523fc07
Trey Shugart <treshugart@gmail.com>
2016-07-14 11:59:31 +1000

    test(ref): Add more tests to ensure refs behave as we want.
    #666

--- test/unit/vdom/ref.js
+++ test/unit/vdom/ref.js
@@ -1,11 +1,17 @@
-import { define, vdom } from '../../../src/index';
+import { define, prop, state, vdom } from '../../../src/index';
 
 describe('vdom/ref', () => {
   function create(ref) {
     const Elem = define('x-test', {
-      render() {
-        vdom.element('div', { ref, id: 'div' }, () => vdom.element('span', { id: 'span' }, 'test'));
-      }
+      props: {
+        num: prop.number(),
+        ref: { initial: () => ref },
+      },
+      render(elem) {
+        vdom.element('div', { ref: elem.ref, id: 'div' }, () => 
+          vdom.element('span', { id: 'span' }, 'test')
+        );
+      },
     });
     return new Elem();
   }
@@ -28,5 +34,33 @@ describe('vdom/ref', () => {
       expect(node.firstChild.textContent).to.equal('test');
       done();
     });
+  });
+
+  it('should be called on every re-render', () => {
+    let num = 0;
+    const elem = create(() => ++num);
+    expect(num).to.equal(1);
+    state(elem, { num: num + 1 });
+    expect(num).to.equal(2);
+    state(elem, { num: num + 1 });
+    expect(num).to.equal(3);
+  });
+
+  it('should call a different ref if changed', () => {
+    let ref1, ref2;
+    const elem = create(() => ref1 = true);
+    expect(ref1).to.equal(true);
+    expect(ref2).to.equal(undefined);
+    state(elem, { ref: () => ref2 = true });
+    expect(ref1).to.equal(true);
+    expect(ref2).to.equal(true);
+  });
+
+  it('should not call a removed ref', () => {
+    let num = 0;
+    const elem = create(() => ++num);
+    expect(num).to.equal(1);
+    state(elem, { ref: null });
+    expect(num).to.equal(1);
   });
 });
