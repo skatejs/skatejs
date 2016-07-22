962f7580a48b0e6ea71fdcd40fbf72c43fd51af5
Trey Shugart <treshugart@gmail.com>
2016-07-22 12:38:02 +1000

    chore(build): Update skatejs-build to 6.0.0.
    This required changing of the rollup and webpack configs.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,1 @@
-const conf = module.exports = require('skatejs-build/rollup.config');
-const pack = require('./package.json');
-const deps = Object.keys(pack.dependencies);
-conf.external = id => deps.indexOf(id) > -1;
-conf.globals = {
-  'incremental-dom': 'IncrementalDOM'
-}
+module.exports = require('skatejs-build/rollup.config');
