31c2d85f8c75bc9a79b8e5f0655efdebb2a78d85
Ben Gummer <bengummer@gmail.com>
2016-07-28 18:37:08 +1000

    chore(tests): switch from SauceLabs to BrowserStack

--- karma.conf.js
+++ karma.conf.js
@@ -2,13 +2,13 @@ const base = require('skatejs-build/karma.conf');
 module.exports = function (config) {
   base(config);
 
-  // Setup IE if testing in SauceLabs.
-  if (config.sauceLabs) {
+  // Setup IE if testing in Browserstack.
+  if (config.browserstack) {
     // Remove all explicit IE definitions.
-    config.browsers = config.browsers.filter(name => !/^internet_explorer/.test(name));
+    // config.browsers = config.browsers.filter(name => !/^internet_explorer/.test(name));
 
     // Only test IE latest.
-    config.browsers.push('internet_explorer_11');
+    // config.browsers.push('internet_explorer_11');
   }
 
   config.files = [
