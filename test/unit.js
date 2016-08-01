e6e554d7d340e1a10a24934bf692ae1401136472
Trey Shugart <treshugart@gmail.com>
2016-08-01 22:05:49 +1000

    test: Add polyfills for v1 custom element polyfill to work.

--- test/unit.js
+++ test/unit.js
@@ -1,9 +1,12 @@
 const div = document.createElement('div');
-const reqCustomElements = require.context('../node_modules/webcomponents.js/src/CustomElements/v1', true, /^.*\.js$/);
+const reqCustomElements = require.context('../node_modules/webcomponents.js/src/CustomElements/v1', true, /^\.\/CustomElements\.js$/);
 const reqNamedSlots = require.context('../node_modules/skatejs-named-slots/dist', true, /^.*\.js$/);
 const reqTests = require.context('./unit', true, /^.*\.js$/);
 
 if (!document.registerElement && !window.customElements) {
+  require('webcomponents.js/src/WeakMap/WeakMap');
+  require('webcomponents.js/src/MutationObserver/MutationObserver');
+  require('es6-shim'); // For Map()
   reqCustomElements('./CustomElements.js');
 }
 
