0a8cd64c203e2efd62b462b1d7b7e4ac8258360d
Trey Shugart <treshugart@gmail.com>
2016-08-11 13:08:07 +1000

    test: Fix tests in Opera 12.
    Fixing involved updating skatejs-named-slots. I ended up just including the new
    
    skatejs-web-components package to simplify allthethings.

--- test/unit.js
+++ test/unit.js
@@ -1,18 +1,10 @@
-const div = document.createElement('div');
-const reqCustomElements = require.context('../node_modules/webcomponents.js/src/CustomElements/v1', true, /^\.\/CustomElements\.js$/);
-const reqNamedSlots = require.context('../node_modules/skatejs-named-slots/dist', true, /^.*\.js$/);
 const reqTests = require.context('./unit', true, /^.*\.js$/);
 
 if (!document.registerElement && !window.customElements) {
-  require('webcomponents.js/src/WeakMap/WeakMap');
-  require('webcomponents.js/src/MutationObserver/MutationObserver');
-  require('es6-shim'); // For Map()
-  reqCustomElements('./CustomElements.js');
-}
-
-if (!div.createShadowRoot && !div.attachShadow) {
-  reqNamedSlots('./index.js');
+  /* eslint import/no-extraneous-dependencies: 0 */
+  require('skatejs-web-components');
 }
 
 require('./boot');
+
 reqTests.keys().map(reqTests);
