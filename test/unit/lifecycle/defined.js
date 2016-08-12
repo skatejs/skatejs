c055209e90db78db4a041467a163813b1be3975f
Trey Shugart <treshugart@gmail.com>
2016-08-12 17:17:26 +1000

    test: Ensure non-upgraded elements don't already have the defined attribute.
    Ensures there's no side-effects happening from other libraries.

--- test/unit/lifecycle/defined.js
+++ test/unit/lifecycle/defined.js
@@ -1,6 +1,10 @@
 import { define } from '../../../src/index';
 
 describe('lifecycle/defined', () => {
+  it('should not already have the defined attribute on undefined elements', () => {
+    expect(document.createElement('some-undefined-element').hasAttribute('defined')).to.equal(false);
+  });
+
   it('should add the [defined] attribute when the element is upgraded', () => {
     const Elem = define('x-test', {});
     const elem = new Elem();
