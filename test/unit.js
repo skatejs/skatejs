3ef13d5b59dd0facae1afa8be7dd155605a34296
Trey Shugart <treshugart@gmail.com>
2016-08-31 10:51:44 +1000

    refactor: Update the test unit.js file to point back to skatejs-web-componenst which will have the r
    #742

--- test/unit.js
+++ test/unit.js
@@ -2,9 +2,7 @@ const reqTests = require.context('./unit', true, /^.*\.js$/);
 
 if (!document.registerElement && !window.customElements) {
   /* eslint import/no-extraneous-dependencies: 0, global-require: 0 */
-  // require('skatejs-web-components');
-  require('skatejs-named-slots');
-  require('document-register-element/build/document-register-element.max');
+  require('skatejs-web-components');
 }
 
 require('./boot');
