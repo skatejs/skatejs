95d0604c3be23612714851a3cfc9f6dbe8d1bacf
Matias Corbanini <mcorbanini@atlassian.com>
2016-09-02 10:18:37 +1000

    refactor: A functional implementation of getAllKeys

--- src/util/get-all-keys.js
+++ src/util/get-all-keys.js
@@ -1,9 +1,7 @@
 
 export default function keys(obj, { enumerable = false } = {}) {
-  const method = enumerable ? 'getOwnPropertyNames' : 'keys';
-  const listOfKeys = Object[method](obj);
-  if (typeof Object.getOwnPropertySymbols === 'function') {
-    listOfKeys.push(...Object.getOwnPropertySymbols(obj));
-  }
-  return listOfKeys;
+  const listOfKeys = Object[enumerable ? 'getOwnPropertyNames' : 'keys'](obj);
+  return typeof Object.getOwnPropertySymbols === 'function' ?
+    listOfKeys.concat(Object.getOwnPropertySymbols(obj)) :
+    listOfKeys;
 }
