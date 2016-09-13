a8b4dd59d6cf3a8f3cb2ab124e9fb86fdb6eccff
Trey Shugart <treshugart@gmail.com>
2016-09-13 21:30:21 +1000

    fix: Fix issue causing property initialisers to fail.
    We were using defineProperty() and defining getters for observedAttributes and props. This caused
    
    errors when using property initialisers as these directly set prop values and if there's no setter
    
    defined, they error out. There's no reason we need to define only getters so we can easily just do
    
    this.
    
    Fixes #771

---
+++ test/unit/api/component.js
@@ -0,0 +1,47 @@
+import { Component } from '../../../src';
+
+describe('api/Component', () => {
+  describe('property getters', () => {
+    it('observedAttributes', () => {
+      class Test extends Component {
+        static get observedAttributes() {
+          return ['test'];
+        }
+      };
+      expect(Test.observedAttributes).to.deep.equal(['test']);
+    });
+
+    it('props', () => {
+      class Test extends Component {
+        static get props() {
+          return {
+            test: { attribute: true }
+          };
+        }
+      };
+      expect(Test.props).to.deep.equal({
+        test: { attribute: true }
+      });
+    });
+  });
+
+  describe('property initialisers', () => {
+    it('observedAttributes', () => {
+      class Test extends Component {
+        static observedAttributes = ['test'];
+      };
+      expect(Test.observedAttributes).to.deep.equal(['test']);
+    });
+
+    it('props', () => {
+      class Test extends Component {
+        static props = {
+          test: { attribute: true }
+        };
+      };
+      expect(Test.props).to.deep.equal({
+        test: { attribute: true }
+      });
+    });
+  });
+});
