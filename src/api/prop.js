ee6772357fd4279c7f39139cb168abbb5b15b0ef
Trey Shugart <treshugart@gmail.com>
2016-09-21 12:24:39 +1000

    refactor: Fix eslint errors.

--- src/api/prop.js
+++ src/api/prop.js
@@ -1,8 +1,8 @@
 import assign from '../util/assign';
 import empty from '../util/empty';
 
-const alwaysUndefinedIfNotANumberOrNumber = (val) => (isNaN(val) ? undefined : Number(val));
-const alwaysUndefinedIfEmptyOrString = (val) => (empty(val) ? undefined : String(val));
+const alwaysUndefinedIfNotANumberOrNumber = val => (isNaN(val) ? undefined : Number(val));
+const alwaysUndefinedIfEmptyOrString = val => (empty(val) ? undefined : String(val));
 
 export function create(def) {
   return (...args) => {
@@ -12,7 +12,7 @@ export function create(def) {
 }
 
 export const array = create({
-  coerce: (val) => (Array.isArray(val) ? val : [val]),
+  coerce: val => (Array.isArray(val) ? val : [val]),
   default: () => [],
   deserialize: JSON.parse,
   serialize: JSON.stringify,
