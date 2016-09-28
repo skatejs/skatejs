ebe36450f859770a0d7b465b1ee4674328dbb539
Luke Batchelor <lbatchelor@atlassian.com>
2016-09-29 09:41:14 +1000

    fix(bug): Changed early exit return logic to just return from inside the for loop

--- src/api/component.js
+++ src/api/component.js
@@ -122,17 +122,15 @@ Component.updated = function updated(elem, prev) {
   if (!prev) {
     return true;
   }
-  let wasUpdated = false;
   // use get all keys so that we check Symbols as well as regular props
   // using a for loop so we can break early
   const allKeys = getAllKeys(prev);
   for (let i = 0; i < allKeys.length; i += 1) {
     if (prev[allKeys[i]] !== elem[allKeys[i]]) {
-      wasUpdated = true;
-      break;
+      return true;
     }
   }
-  return wasUpdated;
+  return false;
 };
 
 Component.prototype = Object.create(HTMLElement.prototype, {
