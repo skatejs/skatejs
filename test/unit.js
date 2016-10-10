5b4f4acef17ae837728d886893ebee0dcda78f70
Ben Gummer <bengummer@gmail.com>
2016-10-10 14:57:16 +1100

    refactor: remove polyfill imported elsewhere

--- test/unit.js
+++ test/unit.js
@@ -1,10 +1,5 @@
 const reqTests = require.context('./unit', true, /^.*\.js$/);
 
-if (!document.registerElement && !window.customElements) {
-  /* eslint import/no-extraneous-dependencies: 0, global-require: 0 */
-  require('skatejs-web-components');
-}
-
 require('./boot');
 
 reqTests.keys().map(reqTests);
