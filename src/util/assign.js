98caa170e65ba66bfe08294b57cc6c41b3405089
Matias Corbanini <mcorbanini@atlassian.com>
2016-10-11 12:56:58 +1100

    fix: assign helper function fix for symbols

--- src/util/assign.js
+++ src/util/assign.js
@@ -1,5 +1,8 @@
-const assign = Object.assign;
-export default assign ? assign.bind(Object) : (obj, ...args) => {
-  args.forEach(arg => Object.keys(arg).forEach(name => obj[name] = arg[name])); // eslint-disable-line no-return-assign
+import keys from './get-all-keys';
+
+// We are not using Object.assign if it is defined since it will cause problems when Symbol is polyfilled.
+// Apparently Object.assign (or any polyfill for this method) does not copy non-native Symbols.
+export default (obj, ...args) => {
+  args.forEach(arg => keys(arg).forEach(name => obj[name] = arg[name])); // eslint-disable-line no-return-assign
   return obj;
 };
--- src/util/get-own-property-descriptors.js
+++ src/util/get-own-property-descriptors.js
@@ -1,7 +1,7 @@
 import getAllKeys from './get-all-keys';
 
 export default function (obj = {}) {
-  return getAllKeys(obj, { enumOnly: false }).reduce((prev, curr) => {
+  return getAllKeys(obj).reduce((prev, curr) => {
     prev[curr] = Object.getOwnPropertyDescriptor(obj, curr);
     return prev;
   }, {});
