f5176880937de624457db5f93af14d9e510e74b2
Joshua Nelson <jonelson@atlassian.com>
2016-10-17 23:12:12 +1100

    test(utils): add failing tests for the fixed utils

--- src/util/native.js
+++ src/util/native.js
@@ -1,6 +1,6 @@
 const nativeHints = [
   'native code',
-  '[object MutationObserverConstructor]', // for mobile safari iOS 9.0
+  '[object MutationObserverConstructor]' // for mobile safari iOS 9.0
 ];
 export default fn => nativeHints.map(
   (hint) => (fn || '').toString().indexOf([hint]) > -1
