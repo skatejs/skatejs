cb6de65237576b242958eead034f3552a0dd215e
Trey Shugart <treshugart@gmail.com>
2016-11-10 16:54:06 +1100

    refactor: Update tests and refactor some code related to the fixes.
    #908

--- test/boot.js
+++ test/boot.js
@@ -1,13 +1,16 @@
 /* eslint-env jasmine, mocha */
 
 import helperFixture from './lib/fixture';
+import '@webcomponents/custom-elements';
+import '@webcomponents/shadydom';
+import '@webcomponents/shadycss';
 
-require('skatejs-web-components');
-
-mocha.setup({ timeout: 10000 }); // eslint-disable-line no-undef
+// eslint-disable-next-line no-undef
+mocha.setup({ timeout: 10000 });
 
 afterEach(() => {
   // Ensure perf tests have enough time to cleanup after themselves.
-  mocha.timeout(120000); // eslint-disable-line no-undef
+  // eslint-disable-next-line no-undef
+  mocha.timeout(120000);
   helperFixture('');
 });
--- test/unit/lifecycle.js
+++ test/unit/lifecycle.js
@@ -50,14 +50,12 @@ describe('lifecycle', () => {
 
   it('should call the detached() callback when the element is detached', (done) => {
     helperFixture().appendChild(myEl);
-    ready(myEl, () => {
-      helperFixture().removeChild(myEl);
-      afterMutations(
-        () => expect(created).to.equal(true, 'created'),
-        () => expect(attached).to.equal(true, 'attached'),
-        () => expect(detached).to.equal(true, 'detached'),
-        done
-      );
-    });
+    afterMutations(
+      () => helperFixture().removeChild(myEl),
+      () => expect(created).to.equal(true, 'created'),
+      () => expect(attached).to.equal(true, 'attached'),
+      () => expect(detached).to.equal(true, 'detached'),
+      done
+    );
   });
 });
