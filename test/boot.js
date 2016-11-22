2ccd4499a04b4989129acb07b050652501bf655a
Trey Shugart <treshugart@gmail.com>
2016-11-22 15:16:10 +1100

    refactor: Add babel parsing to temporary polyfill fix.
    #872

--- test/boot.js
+++ test/boot.js
@@ -1,5 +1,10 @@
 /* eslint-env jasmine, mocha */
 
+// We include these manually because ShadyCSS adds extra output which the tests
+// arent't expecting. We also have to put the native-shim through babel because
+// it's being pulled in from the node_modules directory which is excluded by
+// default.
+import 'babel?presets[]=es2015!skatejs-web-components/src/native-shim';
 import '@webcomponents/custom-elements';
 import '@webcomponents/shadydom';
 import fixture from './lib/fixture';
