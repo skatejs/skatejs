bcd2192bcfdbac6c9e0635ef836011ba9ea7105a
andreawyss <andrea.wyss@gmail.com>
2016-11-22 16:18:30 -0800

    try fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
 module.exports = require('skatejs-build/webpack.config');
 console.log('>>> webpack plugins before', module.exports.module.loaders[2].query.plugins);
-module.exports.module.loaders[2].query.plugins = ['transform-flow-strip-types', 'transform-class-properties'];
+module.exports.module.loaders[2].query.plugins = ['transform-class-properties', 'transform-flow-strip-types'];
 console.log('>>> webpack plugins after', module.exports.module.loaders[2].query.plugins);
