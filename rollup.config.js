2e3a56146b3078df8da56cc6916caa07ea85a696
andreawyss <andrea.wyss@gmail.com>
2016-11-22 16:26:08 -0800

    remove logs

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,14 @@
-const rollupBabel = require('rollup-plugin-babel');
-const presetEs2015 = require('babel-preset-es2015-rollup');
-
-const babel = rollupBabel({
-  presets: presetEs2015,
-  plugins: ['transform-flow-strip-types']
-});
-
 module.exports = require('skatejs-build/rollup.config');
 module.exports.globals = {
   'incremental-dom': 'IncrementalDOM',
   'window-or-global': 'windowOrGlobal'
 };
+
+// This should be part of the default skatejs/build
+const rollupBabel = require('rollup-plugin-babel');
+const presetEs2015 = require('babel-preset-es2015-rollup');
+const babel = rollupBabel({
+  presets: presetEs2015,
+  plugins: ['transform-flow-strip-types']
+});
 module.exports.plugins[0] = babel;
-console.log('>>> rollup', babel);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,2 @@
 module.exports = require('skatejs-build/webpack.config');
-console.log('>>> webpack plugins before', module.exports.module.loaders[2].query.plugins);
 module.exports.module.loaders[2].query.plugins = ['transform-class-properties', 'transform-flow-strip-types'];
-console.log('>>> webpack plugins after', module.exports.module.loaders[2].query.plugins);
