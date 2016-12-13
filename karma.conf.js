79cba817526e960059e5b6bf696a7f5ea8f7fa7a
AndreaWyss <andrea.wyss@gmail.com>
2016-12-13 07:00:03 -0800

    warn of deprecated methods if not in production

--- karma.conf.js
+++ karma.conf.js
@@ -5,11 +5,20 @@
 
 const base = require('skatejs-build/karma.conf');
 const webpackConfig = require('./webpack.config');
+const webpack = require('webpack');
+
 module.exports = (config) => {
   base(config);
 
   // Ensure mobile browsers have enough time to run.
   config.browserNoActivityTimeout = 60000;
+
+  // Override process.env.NODE_ENV
+  webpackConfig.plugins[1] = new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('test')
+      }
+    });
 
   // Since we override the Webpack config, we must make sure Karma gets it.
   config.webpack = Object.assign({}, config.webpack, webpackConfig, {
--- src/util/deprecated.js
+++ src/util/deprecated.js
@@ -1,5 +1,9 @@
+const production = 'production';
+const environment = process.env.NODE_ENV || production;
+
 export default function deprecated (elem, oldUsage, newUsage) {
-  // TODO: add logic to only warn in dev mode
-  // const ownerName = elem.localName ? elem.localName : String(elem);
-  // console.warn(`${ownerName} ${oldUsage} is deprecated. Use ${newUsage}.`);
+  if (environment !== production) {
+    const ownerName = elem.localName ? elem.localName : String(elem);
+    console.warn(`${ownerName} ${oldUsage} is deprecated. Use ${newUsage}.`);
+  }
 }
