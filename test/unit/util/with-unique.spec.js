87cb2fb4449589125e33d2bd71e83f61c1ca28a7
Trey Shugart <treshugart@gmail.com>
2017-03-28 22:35:00 +1100

    refactor: Put with-unique utils in their own spot.

---
+++ test/unit/util/with-unique.spec.js
@@ -0,0 +1,19 @@
+/* eslint-env mocha */
+
+import expect from 'expect';
+
+import { formatName } from 'src/util/with-unique';
+
+describe('{ formatName }', () => {
+  it('should choose a default name', () => {
+    expect(formatName()).toBe('x-element');
+  });
+
+  it('should auto-prefix a name with x- if it contains no dashes', () => {
+    expect(formatName('element')).toBe('x-element');
+  });
+
+  it('should ensure there is a space between the prefix and suffix', () => {
+    expect(formatName('prefix', 'suffix')).toBe('x-prefix-suffix');
+  });
+});
