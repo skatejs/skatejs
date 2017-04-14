5571938d24a7b4335e341d6fcdc5413bb88925d6
Matthew Phillips <matthew@matthewphillips.info>
2017-04-13 22:09:09 -0400

    Get rid of unnecessary extra function

--- src/with-component.js
+++ src/with-component.js
@@ -1,4 +1,4 @@
-import { h, render as preactRender } from 'preact';
+import { h, render } from 'preact';
 
 import { HTMLElement, sym } from './util';
 import { withProps } from './with-props';
@@ -7,16 +7,12 @@ import { withUnique } from './with-unique';
 
 const _preactDom = sym('_preactDom');
 
-function render (vnode, root) {
-  const dom = preactRender(vnode, root, this[_preactDom]);
-  if (!this[_preactDom]) {
-    this[_preactDom] = dom;
-  }
-}
-
 export const withComponent = (Base = HTMLElement) => class extends withUnique(withRender(withProps(Base))) {
   rendererCallback (shadowRoot, renderCallback) {
-    render.call(this, renderCallback(), shadowRoot);
+    const dom = render(renderCallback(), shadowRoot, this[_preactDom]);
+    if (!this[_preactDom]) {
+      this[_preactDom] = dom;
+    }
   }
 };
 
