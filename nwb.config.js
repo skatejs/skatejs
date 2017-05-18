a6fe4c66740803e8a8587053a413aa30dc24e652
Trey Shugart <treshugart@gmail.com>
2017-05-18 10:34:14 -0700

    build: Convert imperative JS into a single command using babel-cli.

--- nwb.config.js
+++ nwb.config.js
@@ -1,33 +1,10 @@
-const babel = require('babel-core');
 const externals = require('webpack-node-externals');
-const fs = require('fs');
-const glob = require('glob');
-const mkdirp = require('mkdirp');
-const path = require('path');
-const rimraf = require('rimraf');
 const yargs = require('yargs');
 
 const browsers = require('./test/browsers');
 
 const [ cmd ] = yargs.argv._;
 const isBuild = cmd === 'build';
-const babelConfig = {
-  plugins: ['transform-react-jsx']
-};
-
-if (isBuild) {
-  const files = glob.sync('src/**/*.js');
-  rimraf.sync('./es');
-  files.forEach(f => {
-    const { code } = babel.transformFileSync(f, Object.assign({}, babelConfig, {
-      presets: ['stage-0', 'es2017', 'es2016']
-    }));
-    const target = f.replace('src/', 'es/');
-    const targetDir = path.dirname(target);
-    mkdirp.sync(targetDir);
-    fs.writeFileSync(target, code);
-  });
-}
 
 module.exports = {
   type: 'web-module',
@@ -35,19 +12,18 @@ module.exports = {
     // CJS is kinda useless if we have UMD.
     cjs: false,
 
-    // We create a custom ESBuild above.
+    // We create a custom ES build using straight-up babel because we can't
+    // have different babel configs for different parts of NWB's builds.
     esModules: false,
 
-    // This is what Node and browsers will consume.
+    // Node and browsers.
     umd: {
       global: 'skate'
     }
   },
   karma: process.argv.indexOf('--ci') === -1 ? {
-    // Local dev.
     browsers: [require('karma-chrome-launcher')]
   } : {
-    // TravisCI (SauceLabs)
     browsers: Object.keys(browsers),
     plugins: ['karma-sauce-launcher'],
     reporters: ['saucelabs'],
@@ -62,18 +38,9 @@ module.exports = {
       captureTimeout: 120000
     }
   },
-  // This specifies the config for all transforms:
-  //
-  // - CJS - ES5 via Babel using require / module.exoorts.
-  // - ESM - Same as CJS but with ESM import / export instead.
-  // - UMD - WebPack
-  //
-  // In order to have different Babel configs (ES latest + UMD) we must only
-  // use this for UMD and have to have some imperative stuff for Babel above
-  // to share the config, otherwise we dupe this.
-  babel: babelConfig,
-
-  // WebPack is used for the UMD build only.
+  babel: {
+    plugins: ['transform-react-jsx']
+  },
   webpack: {
     extra: {
       externals: isBuild ? [externals()] : []
