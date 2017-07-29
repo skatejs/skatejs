9a80c5a900128451a6aec304ce5fa193e6d00003
Trey Shugart <treshugart@gmail.com>
2017-07-29 13:13:02 +1000

    build: Add node build. Minor refactors.

---
+++ config/babel.esnext.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    [
+      require("babel-plugin-modules-map"),
+      {
+        preact: "preact/dist/preact.esm"
+      }
+    ],
+    [
+      require("babel-plugin-modules-web-compat"),
+      {
+        packageResolutionStrategy: "npm"
+      }
+    ]
+  ],
+  presets: ["es2016", "es2017", "react", "stage-0"]
+};
