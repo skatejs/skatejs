a44dd84d044f204f3c2c6996416fb29fce2b48a5
Trey Shugart <treshugart@gmail.com>
2017-08-11 12:26:16 +1000

    docs: Start on some simple components to use in the site.

---
+++ site/components/heading.js
@@ -0,0 +1,8 @@
+/** @jsx h */
+
+import { h } from "../../src";
+
+export const Heading = ({ children }) =>
+  <h1>
+    {children}
+  </h1>;
--- site/index.js
+++ site/index.js
@@ -1,11 +1,30 @@
 /** @jsx h */
 
 import { Component, define, h } from "../src";
+import { Code } from "./components/code";
+import { Heading } from "./components/heading";
 
 export default define(
   class extends Component {
     renderCallback() {
-      return <h1>Testing...</h1>;
+      return (
+        <div>
+          <Heading>SkateJS</Heading>
+          <Code
+            src={`
+            /** @jsx h */
+
+            import { Component, h } from 'skatejs';
+
+            class MyComponent extends Component {
+              renderCallback () {
+                return <div>Hello, <slot />!</div>;
+              }
+            }
+          `}
+          />
+        </div>
+      );
     }
   }
 );
