811cd89128cbae36d1cc454770c31c9e0f2bb7db
Trey Shugart <treshugart@gmail.com>
2017-08-18 16:07:34 +0200

    refactor: Prepare to move preact renderer out.

--- src/with-component.js
+++ src/with-component.js
@@ -1,17 +1,27 @@
 // @flow
 
-import { h, render } from 'preact';
-import { withProps } from './with-props';
-import { withRender } from './with-render';
-import { withUnique } from './with-unique';
+import { h, render } from "preact";
+import { withProps } from "./with-props";
+import { withRender } from "./with-render";
+import { withUnique } from "./with-unique";
 
-export const withComponent = (Base?: Class<any> = HTMLElement): Class<HTMLElement> =>
-  class extends withRender(withUnique(withProps(Base))) {
+const withAll = (Base?: Class<any> = HTMLElement): Class<HTMLElement> =>
+  withProps(withRender(withUnique(Base)));
+
+const withPreact = (Base?: Class<any> = HTMLElement): Class<HTMLElement> =>
+  class extends Base {
     _preactDom: Object;
-    rendererCallback (shadowRoot: Node, renderCallback: () => Object) {
-      this._preactDom = render(renderCallback(), shadowRoot, this._preactDom || shadowRoot.children[0]);
+    rendererCallback(shadowRoot: Node, renderCallback: () => Object) {
+      this._preactDom = render(
+        renderCallback(),
+        shadowRoot,
+        this._preactDom || shadowRoot.children[0]
+      );
     }
   };
 
-export const Component: Class<HTMLElement> = withComponent(typeof window === 'undefined' ? class {} : HTMLElement);
+export const withComponent = withPreact;
+export const Component: Class<HTMLElement> = withAll(
+  withPreact(typeof window === "undefined" ? class {} : HTMLElement)
+);
 export { h };
