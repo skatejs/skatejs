1df43744b3708ad23c34b938945d7c21aa5bd2c1
Trey Shugart <treshugart@gmail.com>
2017-10-28 15:38:39 +1100

    Add tests for context.

--- src/with-context.js
+++ src/with-context.js
@@ -1,21 +1,22 @@
-import { sym } from './util/index';
+// @flow
 
-const _context = sym('context');
+import type { WithContext } from './types';
 
-export const withContext = (Base = HTMLElement) =>
+export const withContext = (
+  Base: Class<any> = HTMLElement
+): Class<WithContext> =>
   class extends Base {
-    get context() {
-      if (this[_context]) {
-        return this[_context];
-      }
+    get context(): Object {
+      if (this._context) return this._context;
       let node = this;
       while ((node = node.parentNode || node.host)) {
         if ('context' in node) {
           return node.context;
         }
       }
+      return {};
     }
-    set context(context) {
-      this[_context] = context;
+    set context(context: Object): void {
+      this._context = context;
     }
   };
---
+++ test/unit/with-context.spec.js
@@ -0,0 +1,28 @@
+// @flow
+
+import { withContext, withUnique } from '../../src';
+
+const Base = withContext(withUnique());
+
+class Test1 extends Base {}
+class Test2 extends Base {}
+
+let test1, test2;
+beforeEach(() => {
+  test1 = new Test1();
+  test2 = new Test2();
+  test1.attachShadow({ mode: 'open' });
+  test1.shadowRoot.appendChild(test2);
+});
+
+test('returns object if no context exists', () => {
+  expect(test1.context).toMatchObject({});
+  expect(test2.context).toMatchObject({});
+});
+
+test('returns context from ancestor', () => {
+  const context = { test: true };
+  test1.context = context;
+  expect(test1.context).toBe(context);
+  expect(test2.context).toBe(context);
+});
