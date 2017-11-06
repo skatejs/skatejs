9f7663f556f54c9451848e91902b40b08c333b52
Trey Shugart <treshugart@gmail.com>
2017-11-06 18:15:47 +1100

    Add custom element and shadow dom fills.

--- site/index.js
+++ site/index.js
@@ -1,5 +1,6 @@
 import 'file-loader!./index.html';
 import '!file-loader?name=ce-es5-shim.js!./ce-es5-shim.js';
+import '!file-loader?name=ce-sd-fill.js!./ce-sd-fill.js';
 import '!file-loader?name=404.html!./index.html';
 import './fills';
 import App from './components/app';
