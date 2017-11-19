2de8a547ffb15c1c1987a2c37e5d7a7e85652886
Martin Hochel <hochelmartin@gmail.com>
2017-11-20 00:40:03 +1100

    Clean any Preact components when CE is removed.

--- packages/renderer-preact/src/__tests__/index.js
+++ packages/renderer-preact/src/__tests__/index.js
@@ -44,3 +44,35 @@ test('wrapper - override render()', () => {
   el.renderer(el, el.render.bind(el));
   expect(el.innerHTML).toMatchSnapshot();
 });
+
+test('wrappers cleanup', () => {
+  const didMountSpy = jest.fn();
+  const willUnmountSpy = jest.fn();
+  class PreactComponent extends Component {
+    componentDidMount = didMountSpy;
+    componentWillUnmount = willUnmountSpy;
+    render() {
+      return <div>Hello, {this.props.children}!</div>;
+    }
+  }
+
+  @define
+  class PreactComponentWrapper extends withRenderer() {
+    render() {
+      return <PreactComponent {...this.props} />;
+    }
+  }
+
+  const root = document.createElement('div');
+  const el = new PreactComponentWrapper();
+  root.appendChild(el);
+  el.renderer(el, el.render.bind(el));
+
+  expect(el.innerHTML).toMatchSnapshot();
+  expect(didMountSpy).toHaveBeenCalled();
+
+  root.removeChild(el);
+
+  expect(el.innerHTML).toMatchSnapshot();
+  expect(willUnmountSpy).toHaveBeenCalled();
+});
