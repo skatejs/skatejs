c31a9332ed1613719ba994c9c7d0fcdafbbd750a
Trey Shugart <treshugart@gmail.com>
2017-12-03 10:42:37 +1100

    Remove remaining traces of TypeScript.

--- projector.js
+++ projector.js
@@ -1,25 +1,11 @@
-const { promisify } = require('util');
 const { getWorkspaces } = require('bolt');
 const execa = require('execa');
 const path = require('path');
-const rmrf = promisify(require('rimraf'));
 
 function exec(...args) {
   return execa(...args)
     .then(r => console.log(r.stdout))
     .catch(console.log);
-}
-
-async function copyTsConfig(w) {
-  return exec('cp', [
-    '-rf',
-    'tsconfig.json',
-    path.join(w.dir, 'tsconfig.json')
-  ]);
-}
-
-async function runTsc(w) {
-  return exec('tsc', ['-p', w.dir]);
 }
 
 async function babel({ envs }) {
@@ -42,16 +28,6 @@ async function babel({ envs }) {
   }
 }
 
-async function ts() {
-  for (const w of await getWorkspaces()) {
-    if ((w.config.files || []).indexOf('ts') === -1) continue;
-    await copyTsConfig(w);
-    await runTsc(w);
-    rmrf(path.join(w.dir, 'tsconfig.json'));
-  }
-}
-
 module.exports = {
-  babel,
-  ts
+  babel
 };
