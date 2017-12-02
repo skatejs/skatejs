813be86035d75372a6df4d82fbb8cef5525a9551
Trey Shugart <treshugart@gmail.com>
2017-12-03 01:09:49 +1100

    TypeScript is a problem I don't want to have anymore. Use something else.

--- packages/skatejs/ts/__tests__/components/custom-base.tsx
+++
@@ -1,4 +0,0 @@
-import { withComponent, WithComponent } from '../..';
-
-export const ButtonComponent = withComponent(HTMLButtonElement);
-export const InputComponent = withComponent(HTMLInputElement);
