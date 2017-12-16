3242264f0ad7ab89bb954596df9bdbbf92429a54
Trey Shugart <treshugart@gmail.com>
2017-12-16 14:21:22 +1100

    define() now uses name() to generate a unique name if a static is property is not found.

--- packages/skatejs/src/define.js
+++ packages/skatejs/src/define.js
@@ -1,8 +1,9 @@
 // @flow
 
 import type { CustomElement } from './types';
+import { name } from './name';
 
 export function define(Ctor: CustomElement): CustomElement {
-  customElements.define(Ctor.is, Ctor);
+  customElements.define(Ctor.is || name(), Ctor);
   return Ctor;
 }
--- packages/skatejs/test/unit/define.spec.js
+++ packages/skatejs/test/unit/define.spec.js
@@ -2,16 +2,19 @@
 
 import { define, name } from '../../src';
 
-describe('api/define', () => {
-  describe('`static is`', () => {
-    it('should be used as the element name', () => {
-      const Elem = define(
-        class extends HTMLElement {
-          static is = name();
-        }
-      );
-      expect(Elem.is).toBeDefined();
-      expect(customElements.get(Elem.is)).toEqual(Elem);
-    });
-  });
+it('should use `static is` as the element name', () => {
+  @define
+  class Elem extends HTMLElement {
+    static is = name();
+  }
+  expect(Elem.is).toBeDefined();
+  expect(customElements.get(Elem.is)).toEqual(Elem);
+});
+
+it('should use `name()` to define a name if `static is` is not defined', () => {
+  @define
+  class Elem extends HTMLElement {}
+  expect(Elem.is).not.toBeDefined();
+  const elem = new Elem();
+  expect(elem.nodeName).toMatch('x-');
 });
