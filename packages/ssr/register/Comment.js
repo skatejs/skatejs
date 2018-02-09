6f09c27dded463f36dcad46309d4281d5a50f900
Trey Shugart <treshugart@gmail.com>
2018-02-09 12:48:00 +1100

    Update comment interface to better match the Text one.
    Eventually we should probably base this off a common interface.

--- packages/ssr/register/Comment.js
+++ packages/ssr/register/Comment.js
@@ -1,11 +1,27 @@
 const { Node } = require('./Node');
 
 const Comment = class extends Node {
-  get outerHTML() {
-    return `<!--${this.textContent}-->`;
+  constructor() {
+    super();
+    this._nodeValue = '';
   }
-  get innerHTML() {
-    return this.textContent;
+  get nodeName() {
+    return '#comment';
+  }
+  get nodeType() {
+    return Node.COMMENT_NODE;
+  }
+  get nodeValue() {
+    return this._nodeValue;
+  }
+  set nodeValue(nodeValue) {
+    this._nodeValue = nodeValue;
+  }
+  get textContent() {
+    return this.nodeValue;
+  }
+  set textContent(textContent) {
+    this.nodeValue = textContent;
   }
 };
 
