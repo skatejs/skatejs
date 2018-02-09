fc903b61ea7f9b48619580d3a845830beebcb599
Trey Shugart <treshugart@gmail.com>
2018-02-09 12:52:00 +1100

    Make tests between hyperhtml and lit-html consistent since they're very similar.

--- packages/renderer-lit-html/src/__tests__/index.js
+++ packages/renderer-lit-html/src/__tests__/index.js
@@ -4,6 +4,10 @@ import withRenderer from '..';
 
 const Component = withRenderer(HTMLElement);
 
+function regexContent(text) {
+  return new RegExp(`Hello, ${text}!`);
+}
+
 @define
 class MyElement extends Component {
   render({ name }) {
@@ -15,7 +19,7 @@ it('renders', () => {
   const el = new MyElement();
   expect(el.innerHTML).toEqual('');
   el.renderer(el, el.render.bind(el, { name: 'World' }));
-  expect(el.innerHTML).toEqual('Hello, <!---->!');
+  expect(el.innerHTML).toMatch(regexContent('World'));
   el.renderer(el, el.render.bind(el, { name: 'Bob' }));
-  expect(el.innerHTML).toEqual('Hello, <!---->!');
+  expect(el.innerHTML).toMatch(regexContent('Bob'));
 });
