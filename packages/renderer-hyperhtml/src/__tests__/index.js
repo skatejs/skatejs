da99a7fcc3480752b20c6ee2ebd86283f456a0c6
Trey Shugart <treshugart@gmail.com>
2018-02-09 12:51:36 +1100

    Fix tests for renderer-hyperhtml

--- packages/renderer-hyperhtml/src/__tests__/index.js
+++ packages/renderer-hyperhtml/src/__tests__/index.js
@@ -3,6 +3,10 @@ import withRenderer from '..';
 
 const Component = withRenderer(HTMLElement);
 
+function regexContent(text) {
+  return new RegExp(`Hello, ${text}<!--_hyper: -?\\d+;-->!`);
+}
+
 @define
 class MyElement extends Component {
   render({ name }) {
@@ -13,8 +17,8 @@ class MyElement extends Component {
 it('renders', () => {
   const el = new MyElement();
   expect(el.innerHTML).toEqual('');
-  // el.renderer(el, el.render.bind(el, { name: 'World' }));
-  // expect(el.innerHTML).toEqual('Hello, <!---->!');
-  // el.renderer(el, el.render.bind(el, { name: 'Bob' }));
-  // expect(el.innerHTML).toEqual('Hello, <!---->!');
+  el.renderer(el, el.render.bind(el, { name: 'World' }));
+  expect(el.innerHTML).toMatch(regexContent('World'));
+  el.renderer(el, el.render.bind(el, { name: 'Bob' }));
+  expect(el.innerHTML).toMatch(regexContent('Bob'));
 });
