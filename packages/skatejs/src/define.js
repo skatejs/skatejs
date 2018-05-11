ab8d55f023c56d37286db48a1dc10e9ac5280226
Trey Shugart <treshugart@gmail.com>
2018-05-11 15:04:05 +1000

    Restore define() function for now.

--- packages/skatejs/src/define.js
+++ packages/skatejs/src/define.js
@@ -4,11 +4,6 @@ import type { CustomElement } from './types.js';
 import { name } from './name.js';
 
 export function define(Ctor: CustomElement): CustomElement {
-  if (!Ctor.is) {
-    Ctor = class extends Ctor {
-      static is = name();
-    };
-  }
-  customElements.define(Ctor.is, Ctor);
+  customElements.define(Ctor.is || name(), Ctor);
   return Ctor;
 }
