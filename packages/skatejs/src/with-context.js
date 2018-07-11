8337adb8755c6131f50b46f5731c2ea567f4acf6
Joe Pea <joe@trusktr.io>
2018-07-11 00:11:35 -0700

    context implementation that doesn't rely on the DOM tree

--- packages/skatejs/src/with-context.js
+++ packages/skatejs/src/with-context.js
@@ -2,19 +2,26 @@
 
 export const withContext = (Base: Class<any> = HTMLElement): Class<any> =>
   class extends Base {
-    get context(): Object {
-      if (this._context) {
-        return this._context;
+    connectedCallback() {
+      super.connectedCallback && super.connectedCallback()
+
+      this._contextCallbacks = new Set
+
+      for (const contextDescriptor in this.constructor.contexts) {
+        const context = contextDescriptor[0]
+        const observedProps = contextDescriptor.slice(1)
+        const callback = () => this.triggerUpdate()
+        this._contextCallbacks.add(callback)
+        context.observe(observedProps, callback)
       }
-      let node = this;
-      while ((node = node.parentNode || node.host)) {
-        if ('context' in node) {
-          return node.context;
-        }
-      }
-      return {};
     }
-    set context(context: Object): void {
-      this._context = context;
+
+    disconnectedCallback() {
+      super.disconnectedCallback && super.disconnectedCallback()
+
+      for (const callback of this._contextCallbacks)
+        context.unobserve(callback)
+
+      this._contextCallbacks = null
     }
-  };
+  }
