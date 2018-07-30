df23cbc8e36670776449a89c63a1370327c56a00
Trey Shugart <treshugart@gmail.com>
2018-07-30 23:36:13 +1000

    Fix flow types in shadow() function.

--- packages/skatejs/src/shadow.js
+++ packages/skatejs/src/shadow.js
@@ -2,6 +2,7 @@
 
 export function shadow(elem: HTMLElement) {
   return (
+    // $FlowFixMe - _shadowRoot isn't in HTMLElement
     elem._shadowRoot ||
     // $FlowFixMe - _shadowRoot isn't in HTMLElement
     (elem._shadowRoot = elem.shadowRoot || elem.attachShadow({ mode: 'open' }))
