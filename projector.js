316e6d9b9d97099e4123899845cc8620d300f2a2
Trey Shugart <treshugart@gmail.com>
2018-08-03 20:14:05 +1000

    Add build utils for calculating release types.

--- projector.js
+++ projector.js
@@ -1,12 +1,15 @@
 const { getWorkspaces } = require('bolt');
+const chalk = require('chalk');
+const charm = require('charm')();
 const execa = require('execa');
 const fs = require('fs-extra');
+const os = require('os');
 const path = require('path');
+const outdent = require('outdent');
+const semver = require('semver');
 
 function exec(...args) {
-  return execa(...args)
-    .then(r => console.log(r.stdout))
-    .catch(console.log);
+  return execa(...args).catch(console.log);
 }
 
 function need(val, msg) {
@@ -15,22 +18,190 @@ function need(val, msg) {
   }
 }
 
+charm.pipe(process.stdout);
+charm.goto = function(pos /*: 'start' | 'end' */) {
+  return this[pos === 'start' ? 'left' : 'right'](100000);
+};
+
+async function getTags() {
+  return (await exec('git', ['tag'])).stdout.split('\n');
+}
+
+async function getTagsFor(name) {
+  return (await getTags()).filter(t => t.indexOf(name) === 0);
+}
+
+async function getLatestTagFor(name) {
+  const tags = await getTagsFor(name);
+  return tags.length ? tags[tags.length - 1] : null;
+}
+
+async function getLatestVersionFromTag(tag) {
+  const parts = tag.split('-');
+  return parts && parts.length ? parts[parts.length - 1] : null;
+}
+
+async function getCommitsSinceTag(tag) {
+  const diff = (await exec('git', ['log', `${tag}...master`, '--oneline']))
+    .stdout;
+  return diff
+    .split('\n')
+    .filter(Boolean)
+    .map(d => {
+      const [hash, ...rest] = d.split(' ');
+      return {
+        hash,
+        message: rest.join(' ')
+      };
+    });
+}
+
+async function getFilesForCommit(commit) {
+  return (await exec('git', [
+    'diff-tree',
+    '--no-commit-id',
+    '--name-only',
+    '-r',
+    commit
+  ])).stdout
+    .split('\n')
+    .filter(Boolean)
+    .map(f => f.replace('/', path.sep));
+}
+
+async function getChangesInWorkspace(workspace) {
+  const latestTag = await getLatestTagFor(workspace.name);
+  if (latestTag) {
+    const commits = await getCommitsSinceTag(latestTag);
+    const mapped = await Promise.all(
+      commits.map(async c => {
+        const files = await getFilesForCommit(c.hash);
+        return files.some(f => {
+          const relativeWorkspaceDir = path.relative(
+            process.cwd(),
+            workspace.dir
+          );
+          return f.indexOf(relativeWorkspaceDir) === 0;
+        })
+          ? c
+          : null;
+      })
+    );
+    return mapped.filter(Boolean);
+  }
+  return [];
+}
+
+function inferReleaseType(message) {
+  const lc = message.toLowerCase();
+  if (lc.includes('breaking')) {
+    return 'major';
+  }
+  if (lc.includes('implements')) {
+    return 'minor';
+  }
+  return 'patch';
+}
+
+function calculateReleaseType(changes) {
+  const weight = { major: 2, minor: 1, patch: 0 };
+  return changes.reduce((currentWeight, change) => {
+    const recommended = inferReleaseType(change.message);
+    if (currentWeight < weight[recommended]) {
+      return weight[recommended];
+    }
+    return currentWeight;
+  }, 'patch');
+}
+
+function calculateNextVersion(version, changes) {
+  return semver.inc(version, calculateReleaseType(changes));
+}
+
 async function babel({ envs }) {
   need(envs, 'Please specify at least one environment.');
-  for (const w of await getWorkspaces()) {
-    for (const env of envs.split(',')) {
-      if ((w.config.files || []).indexOf(`dist/${env}`) === -1) continue;
+
+  const envArr = envs
+    .split(',')
+    .map(s => s.trim())
+    .filter(Boolean);
+  const ws = await getWorkspaces();
+  ws.sort((a, b) => a.name.localeCompare(b.name));
+
+  for (const w of ws) {
+    const files = w.config.files || [];
+    const wname = w.name.replace('@', '');
+
+    charm.write(chalk`\n{white ${wname}}`);
+
+    // If no envs exist for this package, exit early.
+    if (!envArr.every(e => files.some(f => f.match(e)))) {
+      charm.goto('start').write(chalk`{gray ${wname}}`);
+      continue;
+    }
+
+    // Clean up any old build.
+    await fs.remove(path.join(w.dir, 'dist'));
+
+    for (const env of envArr) {
+      charm.write(chalk` {yellow ${env}...}`);
+
+      // Don't build for this env if it's not specified for the package.
+      if (!files.some(f => f.match(env))) {
+        charm
+          .left(env.length + 3)
+          .write(chalk`{cyan ${env}}`)
+          .right(3)
+          .erase('end');
+        continue;
+      }
+
       const src = path.join(w.dir, 'src');
       const dst = path.join(w.dir, 'dist', env);
-      exec('babel', [src, '--out-dir', dst], {
-        env: { BABEL_ENV: env }
-      }).then(() =>
+
+      await Promise.all([
         exec('flow-copy-source', [
           '-i',
           '**/__tests__/**',
           'src',
           `dist/${env}`
-        ])
+        ]),
+        exec('babel', [src, '--out-dir', dst], {
+          env: { BABEL_ENV: env }
+        })
+      ]);
+
+      charm
+        .left(env.length + 3)
+        .write(chalk`{green ${env}}`)
+        .erase('end');
+    }
+  }
+  charm.write(os.EOL).end();
+}
+
+async function changed() {
+  for (const w of await getWorkspaces()) {
+    const changes = await getChangesInWorkspace(w);
+    if (changes.length) {
+      console.log(
+        outdent`
+
+          ${w.config.name} ${chalk`{green ${
+          w.config.version
+        }}`} -> ${chalk`{yellow ${calculateNextVersion(
+          w.config.version,
+          changes
+        )}}`}
+            ${changes
+              .map(
+                d =>
+                  `${d.hash} ${chalk`{yellow ${inferReleaseType(
+                    d.message
+                  )}}`} ${d.message}`
+              )
+              .join(`${os.EOL}  `)}
+        `
       );
     }
   }
@@ -58,5 +229,6 @@ async function release({ packages, type }) {
 
 module.exports = {
   babel,
+  changed,
   release
 };
