2d6d22b62c12dd6ebef4ec9e1381b1cc321032cc
Trey Shugart <treshugart@gmail.com>
2018-08-15 13:34:45 +1000

    Remove redundant tests from removed behaviour.

--- packages/skatejs/src/__tests__/with-component.ts
+++
@@ -1,8 +0,0 @@
-import { withComponent } from '..';
-
-test('should extend custom classes', () => {
-  class Base extends HTMLElement {}
-  expect(class extends withComponent(Base) {}.prototype instanceof Base).toBe(
-    true
-  );
-});
