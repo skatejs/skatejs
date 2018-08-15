51dcd1e8ae655c6fffb521ccab77c1e1293c0b3e
Trey Shugart <treshugart@gmail.com>
2018-08-15 13:34:33 +1000

    Remove shadow() helper. It is now built-in to renderRoot in Component.

--- packages/skatejs/src/index.ts
+++ packages/skatejs/src/index.ts
@@ -3,5 +3,4 @@ export * from './define.js';
 export * from './emit.js';
 export * from './link.js';
 export * from './name.js';
-export * from './shadow.js';
 export * from './types';
--- packages/skatejs/src/shadow.ts
+++
@@ -1,8 +0,0 @@
-import { Root } from './types';
-
-export function shadow(elem: HTMLElement & { _shadowRoot?: Root }): Root {
-  return (
-    elem._shadowRoot ||
-    (elem._shadowRoot = elem.shadowRoot || elem.attachShadow({ mode: 'open' }))
-  );
-}
