d1b26fb91af575c0e4367b57fb3ed7bef839166d
Joe Pea <joe@trusktr.io>
2018-08-28 00:05:59 -0700

    mount to a display:contents container inside the root

--- packages/renderer-snabbdom/src/index.js
+++ packages/renderer-snabbdom/src/index.js
@@ -6,25 +6,22 @@ export default function createMixin(modules) {
   return (Base = HTMLElement) =>
     class extends Base {
       _lastVTree = null;
-        
+
       renderer(root, call) {
         this._renderRoot = root;
         let newVTree = call();
 
-        // first render
         if (!this._lastVTree) {
-          this._lastVTree = patch(root, newVTree);
-        }
-
-        // all subsequent renders
-        else {
-          this._lastVTree = patch(this._lastVTree, newVTree)
+          root.innerHTML = '<div style="display: contents"></div>';
+          this._lastVTree = patch(root.children[0], newVTree);
+        } else {
+          this._lastVTree = patch(this._lastVTree, newVTree);
         }
       }
-      
+
       disconnectedCallback() {
         super.disconnectedCallback && super.disconnectedCallback();
-        patch(this._lastVTree, null)
+        patch(this._lastVTree, null);
       }
     };
 }
