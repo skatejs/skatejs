74976d6a46bc5b9fe290de8a9a80a659a97a94a7
Trey Shugart <treshugart@gmail.com>
2018-09-04 10:07:48 +1000

    @skatejs/sk-router-0.3.0

--- projector.js
+++ projector.js
@@ -275,7 +275,7 @@ async function release({ packages, type }) {
     await exec('npm', ['--no-git-tag-version', 'version', type], { cwd });
     const ver = require(path.join(cwd, 'package.json')).version;
     const tag = `${name}-${ver}`;
-    console.log(w.name, tag);
+    console.log(tag);
     await exec('git', ['commit', '-am', tag], { cwd });
     await exec('git', ['tag', '-a', tag, '-m', tag], { cwd });
     await exec('npm', ['publish'], { cwd });
