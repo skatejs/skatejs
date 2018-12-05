a31c0ac06d7a6fd8d034291eab076796d099e3dc
Trey Shugart <treshugart@gmail.com>
2018-12-05 16:37:09 +1100

    Update unreleased API.

--- packages/define/src/index.ts
+++ packages/define/src/index.ts
@@ -1,2 +1,44 @@
-export { default } from './define';
-export { default as name } from './name';
+export interface Constructor<T> {
+  new (): T;
+  is?: string;
+  name: string;
+}
+
+const ctorToNameMap = new WeakMap();
+
+function dashcase(str: string): string {
+  return str.split(/([_A-Z])/).reduce((one, two, idx) => {
+    const dash = !one || idx % 2 === 0 ? '' : '-';
+    two = two === '_' ? '' : two;
+    return `${one}${dash}${two.toLowerCase()}`;
+  });
+}
+
+function format(prefix: string, suffix: number): string {
+  return (
+    (prefix.indexOf('-') === -1 ? `x-${prefix}` : prefix) +
+    (suffix ? `-${suffix}` : '')
+  );
+}
+
+export default function<T>(ctor: Constructor<T>): Constructor<T> {
+  let currentName = getName(ctor);
+  if (!currentName) {
+    ctorToNameMap.set(ctor, (currentName = generateName(ctor.name)));
+  }
+  if (!customElements.get(currentName)) {
+    customElements.define(currentName, ctor);
+  }
+  return ctor;
+}
+
+export function getName<T>(ctor: Constructor<T>): string | void {
+  return ctor.is || ctorToNameMap.get(ctor);
+}
+
+export function generateName(prefix?: string): string {
+  prefix = dashcase(prefix || 'element');
+  let suffix: number = 0;
+  while (customElements.get(format(prefix, suffix))) ++suffix;
+  return format(prefix, suffix);
+}
