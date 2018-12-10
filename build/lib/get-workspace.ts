c467ed32398ea8db11629b7ba518028502a1e3bc
Trey Shugart <treshugart@gmail.com>
2018-12-11 10:58:19 +1100

    Add build feature for bumping dependants.

---
+++ build/lib/get-workspace.ts
@@ -0,0 +1,15 @@
+import { getWorkspaces } from 'bolt';
+
+export default async function(
+  pkg: string
+): Promise<{
+  [s: string]: any;
+}> {
+  const [w] = (await getWorkspaces()).filter(({ name }) => name === pkg);
+
+  if (!w) {
+    throw new Error(`Could not find workspace ${pkg}.`);
+  }
+
+  return w;
+}
