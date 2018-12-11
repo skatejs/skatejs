f35a85b85b014379fdc8e7a34beb9364fbef2dd4
Trey Shugart <treshugart@gmail.com>
2018-12-11 20:54:13 +1100

    Fix bump script.

--- build/bump.ts
+++ build/bump.ts
@@ -1,37 +1,45 @@
 import { getWorkspaces } from 'bolt';
 import { writeJson } from 'fs-extra';
 import * as memo from 'memoizee';
+import { EOL } from 'os';
 import * as path from 'path';
-import * as semver from 'semver';
 import getDependants from './lib/get-dependants';
 import getWorkspace from './lib/get-workspace';
 
-export default async function() {
+export default async function({ dry, pkg }) {
   const memoGetDependants = memo(getDependants);
 
   for (const w of await getWorkspaces()) {
     const deps = await memoGetDependants(w.name);
 
+    if (!deps.length) {
+      continue;
+    }
+
+    let logged = false;
     for (const dep of deps) {
+      if (pkg && pkg !== dep) {
+        continue;
+      }
+
+      if (!logged) {
+        logged = true;
+        console.log(EOL + w.name);
+      }
+
       const depw = await getWorkspace(dep);
       const depwDeps = depw.config.dependencies;
 
-      if (
-        depwDeps &&
-        depwDeps[w.name] &&
-        !semver.satisfies(w.version, depwDeps[w.name])
-      ) {
+      if (depwDeps && depwDeps[w.name]) {
         console.log(
-          depw.name,
-          w.name,
-          depwDeps[w.name],
-          ' -> ',
-          w.config.version
+          `  ${depw.name}: ${depwDeps[w.name]} -> ${w.config.version}`
         );
 
         depwDeps[w.name] = w.config.version;
 
-        writeJson(path.join(depw.dir, 'package.json'), depw.config);
+        if (!dry) {
+          writeJson(path.join(depw.dir, 'package.json'), depw.config);
+        }
       }
     }
   }
