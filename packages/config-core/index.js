ef9254164187a442acbf0f53760ebac9e422d30a
Trey Shugart <treshugart@gmail.com>
2019-06-22 14:38:30 +1000

    Fix packages.

--- packages/config-core/index.js
+++ packages/config-core/index.js
@@ -1,28 +1,54 @@
-module.exports = {
-  files: [
-    {
-      name: "package.json",
-      merge: true,
-      data: {
-        author: "You <at@you.com>",
-        browser: "module/index.js",
-        bugs: "https://github.com/skatejs/skatejs/issues",
-        dependencies: {},
-        description: "",
-        devDependencies: { jest: "latest", np: "latest" },
-        esnext: "esnext/index.js",
-        files: ["esnext", "main", "module"],
-        homepage: "https://skatejs.netlify.com",
-        keywords: ["web", "components", "custom", "elements", "shadow", "dom"],
-        license: "MIT",
-        main: "main/index.js",
-        module: "module/index.js",
-        name: `@skatejs/package-name`,
-        publishConfig: { access: "public" },
-        repository: "skatejs/skatejs",
-        scripts: { test: "jest" },
-        source: "src/index.ts"
+const info = require("package-info");
+
+module.exports = async () => {
+  // const npInfo = await info("np");
+  // const pikaInfo = await info("@pika/pack");
+
+  return {
+    files: [
+      {
+        name: "package.json",
+        merge: true,
+        data: {
+          author: "You <at@you.com>",
+          bugs: "https://github.com/skatejs/skatejs/issues",
+          description: "",
+          homepage: "https://skatejs.netlify.com",
+          keywords: [
+            "web",
+            "components",
+            "custom",
+            "elements",
+            "shadow",
+            "dom"
+          ],
+          license: "MIT",
+          name: `@skatejs/package-name`,
+          publishConfig: { access: "public" },
+          repository: "skatejs/skatejs",
+          scripts: { test: "jest" }
+        }
+      },
+      {
+        name: "package.json",
+        merge: true,
+        overwrite: true,
+        data: {
+          browser: "module/index.js",
+          dependencies: {},
+          devDependencies: {
+            // "@pika/pack": `^${pikaInfo.version}`,
+            // np: `^${npInfo.version}`,
+            "@pika/pack": undefined,
+            np: undefined
+          },
+          esnext: "esnext/index.js",
+          files: ["esnext", "main", "module"],
+          main: "main/index.js",
+          module: "module/index.js",
+          source: "src/index.ts"
+        }
       }
-    }
-  ]
+    ]
+  };
 };
