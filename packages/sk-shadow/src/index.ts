9a0505e12240638218a48b264823f1ed7bba1a9d
Trey Shugart <treshugart@gmail.com>
2019-06-22 18:45:03 +1000

    Better comments.

--- packages/sk-shadow/src/index.ts
+++ packages/sk-shadow/src/index.ts
@@ -1,4 +1,6 @@
-if (typeof HTMLElement === 'undefined') {
+// On the server, we have to create a dummy HTMLElement so we don't get
+// any errors. We don't need any of the core functionality.
+if (typeof HTMLElement === "undefined") {
   // @ts-ignore
   HTMLElement = class {};
 }
@@ -64,16 +66,18 @@ export default class Shadow extends HTMLElement {
     const { parentElement } = this;
 
     // Ensure it doesn't take up any space.
-    this.style.display = 'contents';
+    this.style.display = "contents";
 
-    // Errors happen if you attach multiple shadow roots. This also allows
-    // detachment and reattachment.
+    // Errors happen if you attach multiple shadow roots, so we must guard
+    // against that. This also allows detachment and reattachment.
     if (parentElement && !parentElement.shadowRoot) {
-      parentElement.attachShadow({ mode: 'open' });
+      parentElement.attachShadow({ mode: "open" });
     }
 
     const { parentElementRoot } = this;
 
+    // This takes all the content in the declared shadow root, and puts
+    // them in the real shadow root. This is rehydration on the client.
     Array.from(super.childNodes).forEach(node => {
       parentElementRoot.appendChild(node as Node);
     });
@@ -83,14 +87,23 @@ export default class Shadow extends HTMLElement {
     const walker = document.createTreeWalker(parentElementRoot);
     while (walker.currentNode) {
       const { currentNode } = walker;
+
+      // We only need to reverse-engineer slotted content.
       if (currentNode instanceof HTMLSlotElement) {
-        if (currentNode.hasAttribute('default')) {
-          currentNode.removeAttribute('default');
-        }
-        while (currentNode.hasChildNodes()) {
-          parentElement.appendChild(currentNode.firstChild);
+        // The "default" attribute is non-standard but we need a way to
+        // declare if the slot's content is the default state or not. If
+        // it has the "default" attribute, we leave content in the slot
+        // alone. If not, we move the content into the parent element so
+        // it gets native slotting.
+        if (currentNode.hasAttribute("default")) {
+          currentNode.removeAttribute("default");
+        } else {
+          while (currentNode.hasChildNodes()) {
+            parentElement.appendChild(currentNode.firstChild);
+          }
         }
       }
+
       walker.nextNode();
     }
   }
