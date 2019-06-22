e315e04f3f8a7b8ef5740ccea57154787f5c7441
Trey Shugart <treshugart@gmail.com>
2019-06-22 16:01:16 +1000

    Update config script.

--- packages/config-core/index.js
+++ packages/config-core/index.js
@@ -1,54 +1,54 @@
 const info = require("package-info");
 
-module.exports = async () => {
-  // const npInfo = await info("np");
-  // const pikaInfo = await info("@pika/pack");
+async function getLatestVersion(name, val) {
+  const version = (await info(name)).version;
+  const fuzzy = version[0] === "0" ? "~" : "^";
+  return fuzzy + version;
+}
 
-  return {
-    files: [
-      {
-        name: "package.json",
-        merge: true,
-        data: {
-          author: "You <at@you.com>",
-          bugs: "https://github.com/skatejs/skatejs/issues",
-          description: "",
-          homepage: "https://skatejs.netlify.com",
-          keywords: [
-            "web",
-            "components",
-            "custom",
-            "elements",
-            "shadow",
-            "dom"
-          ],
-          license: "MIT",
-          name: `@skatejs/package-name`,
-          publishConfig: { access: "public" },
-          repository: "skatejs/skatejs",
-          scripts: { test: "jest" }
-        }
-      },
-      {
-        name: "package.json",
-        merge: true,
-        overwrite: true,
-        data: {
-          browser: "module/index.js",
-          dependencies: {},
-          devDependencies: {
-            // "@pika/pack": `^${pikaInfo.version}`,
-            // np: `^${npInfo.version}`,
-            "@pika/pack": undefined,
-            np: undefined
-          },
-          esnext: "esnext/index.js",
-          files: ["esnext", "main", "module"],
-          main: "main/index.js",
-          module: "module/index.js",
-          source: "src/index.ts"
+function getLatestVersions(depNames) {
+  return depNames.reduce((deps, name) => {
+    deps[name] = getLatestVersion;
+    return deps;
+  }, {});
+}
+
+module.exports = {
+  files: [
+    {
+      name: "package.json",
+      merge: true,
+      data: {
+        author: "You <at@you.com>",
+        bugs: "https://github.com/skatejs/skatejs/issues",
+        description: "",
+        homepage: "https://skatejs.netlify.com",
+        keywords: ["web", "components", "custom", "elements", "shadow", "dom"],
+        license: "MIT",
+        name: `@skatejs/package-name`,
+        publishConfig: {
+          access: "public"
+        },
+        repository: "skatejs/skatejs",
+        scripts: {
+          test: "jest"
         }
       }
-    ]
-  };
+    },
+    {
+      name: "package.json",
+      merge: true,
+      overwrite: true,
+      data: {
+        browser: "module/index.js",
+        dependencies: {},
+        devDependencies: getLatestVersions(["@pika/pack", "np"]),
+        esnext: "esnext/index.js",
+        files: ["esnext", "main", "module"],
+        main: "main/index.js",
+        module: "module/index.js",
+        source: "src/index.ts"
+      }
+    }
+  ]
 };
