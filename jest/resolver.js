c723e1752f90ea28df051ac88391c8ecb96dc662
Trey Shugart <treshugart@gmail.com>
2019-06-28 20:45:52 +1000

    Extract jest resolver into a package.

--- jest/resolver.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-module.exports = (request, options) => {
-  const requestTs = request.replace(".js", ".ts");
-  const resolveTs = fs.existsSync(path.resolve(options.basedir, requestTs));
-  return options.defaultResolver(resolveTs ? requestTs : request, options);
-};
