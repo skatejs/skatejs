559eaed3ca3ae02b6187cc5ff534b2d4649b2664
Trey Shugart <treshugart@gmail.com>
2019-06-30 15:01:50 +1000

    Mess around with new design.

--- next/pages/index.js
+++ next/pages/index.js
@@ -3,40 +3,30 @@
 import Element, { h } from "@skatejs/element-react";
 import css from "@skatejs/shadow-css";
 
+const backgroundColor = "#262434";
+const fontFamily = "helvetica";
+const gridSize = 4;
+const gridUnit = 5;
+const pillColorStart = "F#DF96F";
+const pillColorEnd = "#E94CA2";
+const tabColorStart = "#EC56B0";
+const tabColorEnd = "#6CE9F5";
+const textColor = "#A6A3AB";
+
 const style = css`
-  ${".host"} {
-    border: 1px solid black;
-    display: inline-block;
-    padding: 10px;
+  body {
+    background-color: ${backgroundColor};
+    color: ${textColor};
+    font-family: ${fontFamily};
+    margin: 0;
+    padding: ${gridSize * gridUnit};
   }
 `;
 
-class App extends Element {
-  static get props() {
-    return {
-      count: Number
-    };
-  }
-  constructor() {
-    super();
-    this.count = 0;
-  }
-  connectedCallback() {
-    if (super.connectedCallback) {
-      super.connectedCallback();
-    }
-    setInterval(() => {
-      this.count += 1;
-    }, 1000);
-  }
-  render() {
-    return (
-      <div className={style.host}>
-        <style>{style.toString()}</style>
-        Count: {this.count}
-      </div>
-    );
-  }
-}
-
-export default () => <App />;
+export default () => (
+  <div>
+    <style>{style.toString()}</style>
+    <h1>SkateJS</h1>
+    <p>Hello!</p>
+  </div>
+);
