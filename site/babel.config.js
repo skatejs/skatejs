4e3ecd75f7b449df3e3bc0c7e6e72e634a58ebfe
Trey Shugart <treshugart@gmail.com>
2019-07-12 13:41:07 +1000

    Get everything setup for a next deploy.

--- site/babel.config.js
+++ site/babel.config.js
@@ -1,13 +1,7 @@
 module.exports = {
   plugins: [
     "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-syntax-dynamic-import",
-    [
-      "babel-plugin-prismjs",
-      {
-        languages: ["css", "html", "tsx"]
-      }
-    ]
+    "@babel/plugin-syntax-dynamic-import"
   ],
   presets: [
     [
